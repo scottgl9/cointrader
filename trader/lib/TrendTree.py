927996ac55d8bd3da491bd3be7c7ebcf5a4c8df7
Scott Glover <scottgl@gmail.com>
2018-09-17 11:38:53 -0500

    More updates to TrendTree.

--- trader/lib/TrendTree.py
+++ trader/lib/TrendTree.py
@@ -1,6 +1,7 @@
 from trader.lib.Crossover2 import Crossover2
 
 
+# create root node, and process price/ts updates
 class TrendTreeProcessor(object):
     def __init__(self, indicator, root_node=None):
         self.indicator = indicator
@@ -17,9 +18,10 @@ class TrendTreeProcessor(object):
         value = self.indicator.update(price)
         self.cross_zero.update(value, 0)
 
+        # if trending upward
         if self.cross_zero.crossup_detected():
             self.direction = 1
-            if not self.root_node.created():
+            if not self.root_node.is_created():
                 self.root_node.create(price, self.direction, ts)
             elif self.last_direction != self.direction:
                 child = TrendNode()
@@ -29,9 +31,10 @@ class TrendTreeProcessor(object):
                 self.root_node.update(last_price=price, last_ts=ts)
 
             self.last_direction = self.direction
+        # if trending downward
         elif self.cross_zero.crossdown_detected():
             self.direction = -1
-            if not self.root_node.created():
+            if not self.root_node.is_created():
                 self.root_node.create(price, self.direction, ts)
             elif self.last_direction != self.direction:
                 child = TrendNode()
@@ -41,6 +44,7 @@ class TrendTreeProcessor(object):
                 self.root_node.update(last_price=price, last_ts=ts)
 
             self.last_direction = self.direction
+
 
 class TrendNode(object):
     # create empty TrendNode
@@ -49,15 +53,21 @@ class TrendNode(object):
         self.parent = None
         self.direction = 0
         self.start_price = 0
-        self.last_price = 0
-        self.end_price = 0
         self.start_ts = 0
+        self.last_price = 0
         self.last_ts = 0
-        self.end_ts = 0
         self.created = False
+        self.ended = False
 
-    def created(self):
+    def is_created(self):
         return self.created
+
+    def is_ended(self):
+        return self.ended
+
+    # mark that we are done updating this node
+    def end(self):
+        self.ended = True
 
     # initialize TrendNode
     def create(self, start_price, direction=0, ts=0):
@@ -66,25 +76,21 @@ class TrendNode(object):
         self.start_ts = ts
         self.created = True
 
-    def update(self, last_price=0, last_ts=0, end_price=0, end_ts=0):
+    def update(self, last_price=0, last_ts=0):
+        if not self.created:
+            return
+
         if last_price != 0:
             self.last_price = last_price
         if last_ts != 0:
             self.last_ts = last_ts
-        if end_price != 0:
-            self.end_price = end_price
-        if end_ts != 0:
-            self.end_ts = end_ts
-
 
     def add_child(self, child):
         self.children.append(child)
 
-
     def remove_child(self, child):
         if child in self.children:
             self.children.remove(child)
-
 
     def no_children(self):
         if len(self.children) == 0:
@@ -114,87 +120,37 @@ class TrendNode(object):
     def set_parent(self, parent):
         self.parent = parent
 
+    # if node range is contained within this node range, return True
     def contains(self, node):
-        if self.end_price == 0 and self.last_price != 0:
-            if node.end_price == 0 and node.last_price != 0:
-                if self.start_price < node.start_price and  self.last_price > node.last_price:
-                    return True
-                if self.start_price > node.start_price and  self.last_price < node.last_price:
-                    return True
-            elif node.end_price != 0:
-                if self.start_price < node.start_price and  self.last_price > node.end_price:
-                    return True
-                if self.start_price > node.start_price and  self.last_price < node.end_price:
-                    return True
-
-        elif self.end_price != 0:
-            if node.end_price == 0 and node.last_price != 0:
-                if self.start_price < node.start_price and  self.end_price > node.last_price:
-                    return True
-                if self.start_price > node.start_price and  self.end_price < node.last_price:
-                    return True
-            elif node.end_price != 0:
-                if self.start_price < node.start_price and  self.end_price > node.end_price:
-                    return True
-                if self.start_price > node.start_price and  self.end_price < node.end_price:
-                    return True
+        if self.last_price != 0 and node.last_price != 0:
+            if self.start_price < node.start_price and  self.last_price > node.last_price:
+                return True
+            if self.start_price > node.start_price and  self.last_price < node.last_price:
+                return True
 
         return False
 
-
-    # return True if node extends this TrendNode
+    # return True if node extends this TrendNode, if update is True then also extend this node from node
     def extends(self, node, update=False):
         if self.direction != node.direction:
             return False
 
         if self.direction == 1:
-            if self.end_price == 0 and self.last_price != 0:
-                if node.end_price == 0 and node.last_price != 0:
-                    if self.start_price < node.start_price < self.last_price and node.last_price > self.last_price:
-                        if update:
-                            self.last_price = node.last_price
-                        return True
-                elif node.end_price != 0:
-                    if self.start_price < node.start_price < self.last_price and node.end_price > self.last_price:
-                        if update:
-                            self.last_price = node.end_price
-                        return True
-            elif self.end_price != 0:
-                if node.end_price == 0 and node.last_price != 0:
-                    if self.start_price < node.start_price < self.end_price and node.last_price > self.end_price:
-                        if update:
-                            self.end_price = node.last_price
-                        return True
-                elif node.end_price != 0:
-                    if self.start_price < node.start_price < self.end_price and node.end_price > self.end_price:
-                        if update:
-                            self.end_price = node.end_price
-                        return True
+            if self.last_price != 0 and node.last_price != 0:
+                # if start of node in range of this node, and last of node is outside range of last of this node
+                if self.start_price < node.start_price < self.last_price and node.last_price > self.last_price:
+                    if update:
+                        self.last_price = node.last_price
+                    return True
         elif self.direction == -1:
-            if self.end_price == 0 and self.last_price != 0:
-                if node.end_price == 0 and node.last_price != 0:
-                    if self.start_price > node.start_price > self.last_price and node.last_price < self.last_price:
-                        if update:
-                            self.last_price = node.last_price
-                        return True
-                elif node.end_price != 0:
-                    if self.start_price < node.start_price < self.last_price and node.end_price < self.last_price:
-                        if update:
-                            self.last_price = node.end_price
-                        return True
-            elif self.end_price != 0:
-                if node.end_price == 0 and node.last_price != 0:
-                    if self.start_price > node.start_price > self.end_price and node.last_price < self.end_price:
-                        if update:
-                            self.end_price = node.last_price
-                        return True
-                elif node.end_price != 0:
-                    if self.start_price > node.start_price > self.end_price and node.end_price < self.end_price:
-                        if update:
-                            self.end_price = node.end_price
-                        return True
-        return False
+            if self.last_price != 0 and node.last_price != 0:
+                # if start of node in range of this node, and last of node is outside range of last of this node
+                if self.start_price > node.start_price > self.last_price and node.last_price < self.last_price:
+                    if update:
+                        self.last_price = node.last_price
+                    return True
 
+        return False
 
     # extend self with node
     def extend(self, node):
@@ -206,19 +162,26 @@ class TrendTree(TrendNode):
         if root_node:
             self.root_node = root_node
         else:
-            super(TrendTree, self).__init__()
+            self.root_node = super(TrendTree, self).__init__()
 
     def set_root(self, node):
         self.root_node = node
 
-    def created(self):
-        return self.root_node.created()
+    def is_created(self):
+        return self.root_node.is_created()
+
+    def is_ended(self):
+        return self.root_node.is_ended()
+
+    # mark that we are done updating this node
+    def end(self):
+        self.root_node.end()
 
     def create(self, start_price, direction=0, ts=0):
         self.root_node.create(start_price, direction, ts)
 
-    def update(self, last_price=0, last_ts=0, end_price=0, end_ts=0):
-        self.root_node.update(last_price, last_ts, end_price, end_ts)
+    def update(self, last_price=0, last_ts=0):
+        self.root_node.update(last_price, last_ts)
 
     def add_child(self, child):
         return self.root_node.add_child(child)
@@ -255,4 +218,3 @@ class TrendTree(TrendNode):
 
     def extend(self, node):
         return self.root_node.extend(node)
-
