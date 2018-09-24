027c7248620018586d3a6b8f2464ac7d1f2aec7e
Scott Glover <scottgl@gmail.com>
2018-09-23 21:52:20 -0500

    Added FREMA indicator.

---
+++ trader/indicator/ehler/FREMA.py
@@ -0,0 +1,35 @@
+# Forward Reverse EMA (FREMA)
+# https://www.prorealcode.com/prorealtime-indicators/john-ehlers-forward-reverse-ema/
+import numpy as np
+from trader.lib.CircularArray import CircularArray
+
+class FREMA(object):
+    def __init__(self):
+        self.AA = 0.1
+        self.CC = 1 - self.AA
+        self.result = 0
+        self.RE1 = CircularArray(window=3, reverse=True, dne=0)
+        self.RE2 = CircularArray(window=3, reverse=True, dne=0)
+        self.RE3 = CircularArray(window=3, reverse=True, dne=0)
+        self.RE4 = CircularArray(window=3, reverse=True, dne=0)
+        self.RE5 = CircularArray(window=3, reverse=True, dne=0)
+        self.RE6 = CircularArray(window=3, reverse=True, dne=0)
+        self.RE7 = CircularArray(window=3, reverse=True, dne=0)
+        self.RE8 = CircularArray(window=3, reverse=True, dne=0)
+        self.EMA = CircularArray(window=3, reverse=True, dne=0)
+        self.Signal = CircularArray(window=3, reverse=True, dne=0)
+
+    def update(self, close):
+        self.EMA.add(self.AA * float(close) + self.CC * self.EMA[1])
+        self.RE1.add(self.CC * self.EMA[0] + self.EMA[1])
+        self.RE2.add(np.exp(2 * np.log(CC)) * self.RE1[0] + self.RE1[1])
+        self.RE3.add(np.exp(4 * np.log(CC)) * self.RE2[0] + self.RE2[1])
+        self.RE4.add(np.exp(8 * np.log(CC)) * self.RE3[0] + self.RE3[1])
+        self.RE5.add(np.exp(16 * np.log(CC)) * self.RE4[0] + self.RE4[1])
+        self.RE6.add(np.exp(32 * np.log(CC)) * self.RE5[0] + self.RE5[1])
+        self.RE7.add(np.exp(64 * np.log(CC)) * self.RE6[0] + self.RE6[1])
+        self.RE8.add(np.exp(128 * np.log(CC)) * self.RE7[0] + self.RE7[1])
+        self.Signal.add(self.EMA[0] - self.AA * self.RE8[0])
+        self.result = self.Signal[0]
+
+        return self.result
