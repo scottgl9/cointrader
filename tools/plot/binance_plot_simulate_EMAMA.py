ef7e152e37532d151ffade673eee7ada5076a377
Scott Glover <scottgl@gmail.com>
2018-10-12 14:48:00 -0500

    Added new indicator I designed called DTWMA. Also added binance_plot_simulate_DTWMA and binance_plot_simulate_EMAMA.

---
+++ tools/plot/binance_plot_simulate_EMAMA.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+
+import sys
+try:
+    import trader
+except ImportError:
+    sys.path.append('.')
+    import trader
+
+import os.path
+import time
+import sqlite3
+from datetime import datetime, timedelta
+from pypika import Query, Table, Field, Order
+from trader.strategy import *
+from datetime import datetime, timedelta
+import threading
+import sys
+from trader.WebHandler import WebThread
+from trader.account.binance.client import Client
+from trader.MultiTrader import MultiTrader
+from trader.account.AccountBinance import AccountBinance
+from trader.config import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from trader.indicator.OBV import OBV
+from trader.indicator.ehler.EMAMA import EMAMA
+from trader.lib.FakeKline import FakeKline
+from trader.indicator.ZLEMA import *
+
+
+def get_rows_as_msgs(c):
+    msgs = []
+    for row in c:
+        msg = {'E': row[0], 'c': row[1], 'h': row[2], 'l': row[3],
+               'o': row[4], 'q': row[5], 's': row[6], 'v': row[7]}
+        msgs.append(msg)
+    return msgs
+
+
+def simulate(conn, client, base, currency, type="channel"):
+    ticker_id = "{}{}".format(base, currency)
+    c = conn.cursor()
+    #c.execute("SELECT * FROM miniticker WHERE s='{}' ORDER BY E ASC".format(ticker_id))
+    c.execute("SELECT E,c,h,l,o,q,s,v FROM miniticker WHERE s='{}'".format(ticker_id)) # ORDER BY E ASC")")
+
+    obv_ema12 = DZLEMA(12, scale=24) #EMA(12, scale=24)
+    obv_ema26 = DZLEMA(26, scale=24) #EMA(26, scale=24)
+    obv_ema50 = DZLEMA(50,scale=24) #EMA(50, scale=24, lag_window=5)
+    fkline = FakeKline()
+    obv_ema12_values = []
+    obv_ema26_values = []
+    obv_ema50_values = []
+
+    obv = OBV()
+    close_prices = []
+    open_prices = []
+    low_prices = []
+    high_prices = []
+    volumes = []
+
+    emama = EMAMA()
+    emama_x_values = []
+    emama_values = []
+    efama_values = []
+
+    i=0
+    for msg in get_rows_as_msgs(c):
+        close = float(msg['c'])
+        low = float(msg['l'])
+        high = float(msg['h'])
+        open = float(msg['o'])
+        volume = float(msg['v'])
+        #sar_value = sar.update(close=close, low=low, high=high)
+        #if sar.bull:
+        #    sar_x_values.append(i)
+        #    sar_values.append(sar_value)
+
+        volumes.append(volume)
+
+        obv_value = obv.update(close=close, volume=volume)
+        obv_ema12_values.append(obv_ema12.update(obv_value))
+        obv_ema26_values.append(obv_ema26.update(obv_value))
+        obv_ema50_values.append(obv_ema50.update(obv_value))
+
+        emama_value, efama_value = emama.update(close)
+        if emama_value != 0:
+            emama_values.append(emama_value)
+            efama_values.append(efama_value)
+            emama_x_values.append(i)
+
+        close_prices.append(close)
+        open_prices.append(open)
+        low_prices.append(low)
+        high_prices.append(high)
+        #lstsqs_x_values.append(i)
+        i += 1
+
+    #plt.subplot(211)
+    fig = plt.figure()
+    ax = fig.add_subplot(2,1,1)
+
+    symprice, = plt.plot(close_prices, label=ticker_id)
+    fig1, = plt.plot(emama_x_values, emama_values, label='EMAMA')
+    fig2, = plt.plot(emama_x_values, efama_values, label='EFAMA')
+    plt.legend(handles=[symprice, fig1, fig2])
+    plt.subplot(212)
+    fig21, = plt.plot(obv_ema12_values, label='OBV12')
+    fig22, = plt.plot(obv_ema26_values, label='OBV26')
+    fig23, = plt.plot(obv_ema50_values, label='OBV50')
+    plt.legend(handles=[fig21, fig22, fig23])
+    #plt.plot(signal_values)
+    #plt.plot(tsi_values)
+    plt.show()
+
+if __name__ == '__main__':
+    client = Client(MY_API_KEY, MY_API_SECRET)
+
+    base = 'BTC'
+    currency='USDT'
+    filename = 'cryptocurrency_database.miniticker_collection_04092018.db'
+    if len(sys.argv) == 4:
+        base=sys.argv[1]
+        currency = sys.argv[2]
+        filename = sys.argv[3]
+    if len(sys.argv) == 3:
+        base=sys.argv[1]
+        currency = sys.argv[2]
+
+    print("Loading {}".format(filename))
+    conn = sqlite3.connect(filename)
+
+    simulate(conn, client, base, currency, type="MACD")
+    conn.close()
---
+++ trader/indicator/test/DTWMA.py
@@ -0,0 +1,36 @@
+# Indicator I designed called Delta Time/Timestamp Weighted Moving Average
+from trader.lib.CircularArray import CircularArray
+
+class DTWMA(object):
+    def __init__(self, window):
+        self.window = window
+        self.timestamps = CircularArray(window=window)
+        self.ts_deltas = CircularArray(window=window)
+        self.prices = CircularArray(window=window)
+        self.result = 0
+
+    def update(self, price, ts):
+        self.prices.add(price)
+
+        if not self.timestamps.empty():
+            self.ts_deltas.add(ts - self.timestamps.last())
+
+        self.timestamps.add(ts)
+
+        if not self.ts_deltas.full():
+            self.result = price
+            return self.result
+
+        deltas = self.ts_deltas.values_ordered()
+        prices = self.prices.values_ordered()
+
+        total = 0
+        avg_price = 0
+
+        for i in range(0, self.window - 1):
+            avg_price += deltas[i] * prices[i]
+            total += deltas[i]
+
+        self.result = avg_price / total
+
+        return self.result
