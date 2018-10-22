18ff311938398541d7f602b3ef7da2b53fb6adbd
Scott Glover <scottgl@gmail.com>
2018-10-22 16:43:58 -0500

    Added binance_plot_simulate_VolumeTests.

---
+++ tools/plot/binance_plot_simulate_VolumeTests.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+
+import sys
+try:
+    import trader
+except ImportError:
+    sys.path.append('.')
+    import trader
+
+import sqlite3
+import sys
+import numpy as np
+from trader.account.binance.client import Client
+from trader.config import *
+import matplotlib.pyplot as plt
+from trader.indicator.EMA import EMA
+from trader.indicator.ZLEMA import ZLEMA
+from trader.indicator.DTWMA import DTWMA
+
+def get_rows_as_msgs(c):
+    msgs = []
+    for row in c:
+        msg = {'E': row[0], 'c': row[1], 'h': row[2], 'l': row[3],
+               'o': row[4], 'q': row[5], 's': row[6], 'v': row[7]}
+        msgs.append(msg)
+    return msgs
+
+
+def simulate(conn, client, base, currency, type="channel"):
+    ticker_id = "{}{}".format(base, currency)
+    c = conn.cursor()
+    #c.execute("SELECT * FROM miniticker WHERE s='{}' ORDER BY E ASC".format(ticker_id))
+    c.execute("SELECT E,c,h,l,o,q,s,v FROM miniticker WHERE s='{}'".format(ticker_id)) # ORDER BY E ASC")")
+
+    ema12 = EMA(12, scale=24)
+    ema26 = EMA(26, scale=24)
+    ema50 = EMA(100, scale=24, lag_window=5)
+    ema200 = EMA(200, scale=24, lag_window=5)
+
+    dtwma = DTWMA(window=30)
+
+    ema_volume = ZLEMA(10)
+
+    vol_ema12 = ZLEMA(12, scale=24)
+    vol_ema26 = ZLEMA(50, scale=24)
+    vol_ema12_values = []
+    vol_ema26_values = []
+
+    ema12_values = []
+    ema26_values = []
+    ema50_values = []
+    ema200_values = []
+    close_prices = []
+    open_prices = []
+    low_prices = []
+    diff_values = []
+    signal_values = []
+    high_prices = []
+    volumes = []
+
+    i=0
+    for msg in get_rows_as_msgs(c):
+        close = float(msg['c'])
+        low = float(msg['l'])
+        high = float(msg['h'])
+        open = float(msg['o'])
+        volume = float(msg['v'])
+        ts=int(msg['E'])
+
+        ema12_value = ema12.update(close)
+        ema12_values.append(ema12_value)
+        ema26_values.append(ema26.update(close))
+        ema50_value = ema50.update(close)
+        ema50_values.append(ema50_value)
+        ema200_value = ema200.update(close)
+        ema200_values.append(ema200_value)
+
+        dtwma.update(close, ts)
+
+        volume = ema_volume.update(volume)
+
+        logvol = float(np.log(volume))
+        if not np.isnan(logvol) and logvol != -np.inf:
+            vol_ema12.update(float(logvol))
+            vol_ema26.update(float(logvol))
+            vol_ema12_values.append(vol_ema12.result)
+            vol_ema26_values.append(vol_ema26.result)
+            volumes.append(logvol)
+
+        close_prices.append(close)
+        open_prices.append(open)
+        low_prices.append(low)
+        high_prices.append(high)
+        #lstsqs_x_values.append(i)
+        i += 1
+
+    plt.subplot(211)
+    symprice, = plt.plot(close_prices, label=ticker_id)
+    fig1, = plt.plot(ema12_values, label='EMA12')
+    fig2, = plt.plot(ema26_values, label='EMA26')
+    fig3, = plt.plot(ema50_values, label='EMA50')
+    fig4, = plt.plot(ema200_values, label='EMA200')
+    plt.legend(handles=[symprice, fig1, fig2, fig3, fig4])
+    plt.subplot(212)
+    fig21, = plt.plot(volumes, label='VOLUME')
+    fig22, = plt.plot(vol_ema12_values, label="EMAVOL12")
+    fig23, = plt.plot(vol_ema26_values, label="EMAVOL26")
+
+    #plt.plot(bb_low_values)
+    plt.legend(handles=[fig21,fig22,fig23])
+    #plt.plot(signal_values)
+    #plt.plot(tsi_values)
+    plt.show()
+
+if __name__ == '__main__':
+    client = Client(MY_API_KEY, MY_API_SECRET)
+
+    base = 'BTC'
+    currency='USDT'
+    filename = 'cryptocurrency_database.miniticker_collection_04092018.db'
+    if len(sys.argv) == 4:
+        base=sys.argv[1]
+        currency = sys.argv[2]
+        filename = sys.argv[3]
+    if len(sys.argv) == 3:
+        base=sys.argv[1]
+        currency = sys.argv[2]
+
+    print("Loading {}".format(filename))
+    conn = sqlite3.connect(filename)
+
+    simulate(conn, client, base, currency, type="MACD")
+    conn.close()
