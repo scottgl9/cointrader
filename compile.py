862b050d1f3e537dee3e9d4893f3b82ec8fafc22
Scott Glover <scottgl@gmail.com>
2018-11-19 17:33:50 -0600

    Updated compile.py

--- compile.py
+++ compile.py
@@ -5,22 +5,43 @@ from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 import glob
 import sys
+import os
 
-if sys.platform == 'win32':
-    indicator_files=glob.glob("trader\\indicator\\*.py")
-    signal_files=glob.glob("trader\\signal\\*.py")
-else:
-    indicator_files=glob.glob("trader/indicator/*.py")
-    signal_files=glob.glob("trader/signal/*.py")
+# scan the 'dvedit' directory for extension files, converting
+# them to extension names in dotted notation
+def scandir(dir, files=[]):
+    for file in os.listdir(dir):
+        path = os.path.join(dir, file)
+        if os.path.isfile(path) and path.endswith(".py"):
+            files.append(path.replace(os.path.sep, ".")[:-4])
+        elif os.path.isdir(path):
+            scandir(path, files)
+    return files
 
-#ext_modules = [
-#    Extension("trader.indicator",  indicator_files),
-#    Extension("trader.signal",  signal_files),
-#   ... all your modules that need be compiled ...
-#]
 
+# generate an Extension object from its dotted name
+def makeExtension(extName):
+    extPath = extName.replace(".", os.path.sep)+".py"
+    return Extension(
+        extName,
+        [extPath],
+        include_dirs = ["."],   # adding the '.' to include_dirs is CRUCIAL!!
+        extra_compile_args = ["-O3", "-Wall"],
+        extra_link_args = ['-g'],
+        #libraries = ["dv",],
+        )
+
+# get the list of extensions
+extNames = scandir("trader")
+
+# and build up the set of Extension objects
+extensions = [makeExtension(name) for name in extNames]
+
+# finally, we can pass all this to distutils
 setup(
-    name = 'trader',
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = cythonize("trader/indicator/*.py")
+  name="trader",
+  packages=["trader", "trader.indicators", "trader.lib", "trader.signal", "trader.strategy",
+            "trader.account", "trader.account.binance", "trader.strategy.trader_size_strategy"],
+  ext_modules=extensions,
+  cmdclass = {'build_ext': build_ext},
 )
