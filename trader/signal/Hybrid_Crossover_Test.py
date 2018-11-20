5a32c57f4eaf4f6b69c0b10116b75297ad3994c8
Scott Glover <scottgl@gmail.com>
2018-11-20 17:47:06 -0600

    More updates to Hybrid_Crossover_Test.

--- trader/signal/Hybrid_Crossover_Test.py
+++ trader/signal/Hybrid_Crossover_Test.py
@@ -33,19 +33,12 @@ class Hybrid_Crossover_Test(SignalBase):
         self.ema200 = EMA(200, scale=24, lag_window=5)
 
         cross_timeout = 1000 * 300
-        self.ema_cross_10_30 = MACross(ema_win1=10, ema_win2=30, scale=24, cross_timeout=cross_timeout)
-        self.ema_cross_30_50 = MACross(ema_win1=30, ema_win2=50, scale=24, cross_timeout=cross_timeout)
+        self.ema_cross_12_26 = MACross(ema_win1=12, ema_win2=26, scale=24, cross_timeout=cross_timeout)
+        self.ema_cross_26_50 = MACross(ema_win1=26, ema_win2=50, scale=24, cross_timeout=cross_timeout)
         self.ema_cross_50_100 = MACross(ema_win1=50, ema_win2=100, scale=24, cross_timeout=cross_timeout)
         self.ema_cross_100_200 = MACross(ema_win1=100, ema_win2=200, scale=24, cross_timeout=cross_timeout)
 
-        self.cross_long = Crossover2(window=10, cutoff=0.0)
-        self.obv_cross = Crossover2(window=10, cutoff=0.0)
-        self.trend_down_count = 0
-        self.trend_up_count = 0
-        self.trending_up = False
-        self.trending_down = False
-        self.obv_trending_up = False
-        self.obv_trending_down = False
+        self.obv_ema_cross_12_26 = MACross(ema_win1=12, ema_win2=26, scale=24) #, cross_timeout=cross_timeout)
 
     def pre_update(self, close, volume, ts):
         if self.timestamp == 0:
@@ -61,66 +54,47 @@ class Hybrid_Crossover_Test(SignalBase):
         #dtwma_volume = self.dtwma_volume.result
 
         self.obv.update(close=close, volume=volume)
-        self.obv_ema12.update(self.obv.result)
-        self.obv_ema26.update(self.obv.result)
-        self.obv_ema50.update(self.obv.result)
+        self.obv_ema_cross_12_26.update(self.obv.result, ts)
 
-        self.ema_cross_10_30.update(close, ts)
-        self.ema_cross_30_50.update(close, ts)
+        self.ema_cross_12_26.update(close, ts)
+        self.ema_cross_26_50.update(close, ts)
         self.ema_cross_50_100.update(close, ts)
         self.ema_cross_100_200.update(close, ts)
 
         self.ema100.update(close)
         self.tpv.update(self.ema100.result, ts)
-        self.detector.update(self.ema_cross_30_50.get_ma2_result())
+        self.detector.update(self.ema_cross_26_50.get_ma2_result())
 
         self.ema12.update(close)
         self.ema26.update(close)
         self.ema50.update(close)
         self.ema200.update(close)
-        self.cross_long.update(self.ema50.result, self.ema200.result)
-
-        if self.cross_long.crossup_detected():
-            self.trending_up = True
-            self.trending_down = False
-            self.last_cross_ts = ts
-        elif self.cross_long.crossdown_detected():
-            self.trending_down = True
-            self.trending_up = False
-            self.last_cross_ts = ts
-
-        self.obv_cross.update(self.obv_ema26.result, self.obv_ema50.result)
-
-        if self.obv_cross.crossup_detected():
-            self.obv_trending_up = True
-            self.obv_trending_down = False
-            self.last_obv_cross_ts = ts
-        elif self.obv_cross.crossdown_detected():
-            self.obv_trending_up = False
-            self.obv_trending_down = True
-            self.last_obv_cross_ts = ts
+        #self.cross_long.update(self.ema50.result, self.ema200.result)
 
     def buy_signal(self):
         if self.last_sell_ts != 0 and (self.timestamp - self.last_sell_ts) < 1000 * 800:
             return False
 
         # it has been over an hour since last crossover, and last crossover was a cross down
-        if (self.trending_down and self.last_cross_ts != 0 and (self.timestamp - self.last_cross_ts) > (3600 * 1000)):
-            return False
+        #if (self.trending_down and self.last_cross_ts != 0 and (self.timestamp - self.last_cross_ts) > (3600 * 1000)):
+        #    return False
 
-        if (self.obv_trending_down and self.last_obv_cross_ts != 0 and (self.timestamp - self.last_obv_cross_ts) > (3600 * 1000)):
-            return False
+        #if (self.obv_trending_down and self.last_obv_cross_ts != 0 and (self.timestamp - self.last_obv_cross_ts) > (3600 * 1000)):
+        #    return False
 
-        if self.obv_ema26.result < self.obv_ema26.last_result and self.ema26.result < self.ema26.last_result:
-            return False
+        #if self.obv_ema26.result < self.obv_ema26.last_result and self.ema26.result < self.ema26.last_result:
+        #    return False
 
-        if self.obv_ema12.result < self.obv_ema12.last_result and self.ema12.result < self.ema12.last_result:
-            return False
+        #if self.obv_ema12.result < self.obv_ema12.last_result and self.ema12.result < self.ema12.last_result:
+        #    return False
 
-        if self.ema_cross_50_100.cross_up and self.ema_cross_30_50.cross_up:
+        #if not self.obv_ema_cross_12_26.cross_up:
+        #    return False
+
+        if self.ema_cross_50_100.cross_up and self.ema_cross_26_50.cross_up:
             return True
 
-        if self.ema_cross_10_30.cross_up and self.ema_cross_30_50.cross_up:
+        if self.ema_cross_12_26.cross_up and self.ema_cross_26_50.cross_up:
             return True
 
         #if self.detector.valley_detect():
@@ -142,19 +116,10 @@ class Hybrid_Crossover_Test(SignalBase):
         if self.ema_cross_50_100.cross_down:
             return True
 
-        if self.ema_cross_30_50.cross_down:
+        if self.ema_cross_26_50.cross_down:
             return True
 
-        if self.ema_cross_10_30.cross_down:
-            return True
-
-        if (self.trending_down and self.last_cross_ts != 0 and (self.timestamp - self.last_cross_ts) > (3600 * 1000)):
-            return True
-
-        if (self.obv_trending_down and self.last_obv_cross_ts != 0 and (self.timestamp - self.last_obv_cross_ts) > (3600 * 1000)):
-            return True
-
-        if self.obv_ema26.result < self.obv_ema26.last_result and self.ema26.result < self.ema26.last_result:
+        if self.ema_cross_12_26.cross_down:
             return True
 
         #if self.detector.peak_detect():
