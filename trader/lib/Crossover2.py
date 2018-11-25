83faf5e4f6fb64550867faf9bffc1c290313c9da
Scott Glover <scottgl@gmail.com>
2018-11-25 01:13:29 -0600

    Fixed update_values1_min_max() and update_values2_min_max() to correctly act as high performance min/max for values1 and values2.

--- trader/lib/Crossover2.py
+++ trader/lib/Crossover2.py
@@ -9,6 +9,7 @@ class Crossover2(object):
         self.values1 = []
         self.values2 = []
         self.age = 0
+        self.last_age = 0
         self.values_under = False
         self.values_over = False
         self.crossup = False
@@ -16,15 +17,15 @@ class Crossover2(object):
 
         # optimize check for min and max of self.values1
         self.values1_min_value = 0
-        self.values1_min_index = -1
+        self.values1_min_age = 0
         self.values1_max_value = 0
-        self.values1_max_index = -1
+        self.values1_max_age = 0
 
         # optimize check for min and max of self.values2
         self.values2_min_value = 0
-        self.values2_min_index = -1
+        self.values2_min_age = 0
         self.values2_max_value = 0
-        self.values2_max_index = -1
+        self.values2_max_age = 0
 
     def update(self, value1, value2):
         if len(self.values1) < self.window or len(self.values2) < self.window:
@@ -36,79 +37,91 @@ class Crossover2(object):
             self.update_values1_min_max(float(value1))
             self.update_values2_min_max(float(value2))
 
-            if self.values_under and min(self.values1) > max(self.values2):
+            if self.values_under and self.values1_min_value > self.values2_max_value:
                 # values1 in window were under values2, now all values1 over values2
                 self.values_under = False
                 self.crossup = True
-            elif self.values_over and max(self.values1) < min(self.values2):
+            elif self.values_over and self.values1_max_value < self.values2_min_value:
                 # values1 in window were over values2, now all values1 under values2
                 self.values_over = False
                 self.crossdown = True
             elif not self.values_under and not self.values_over:
-                if max(self.values1) < min(self.values2):
+                if self.values1_max_value < self.values2_min_value:
                     # all of values1 under values2
                     self.values_under = True
-                elif min(self.values1) > max(self.values2):
+                elif self.values1_min_value > self.values2_max_value:
                     # all of values1 over values2
                     self.values_over = True
 
-        self.age = (self.age + 1) % self.window
+            self.last_age = self.age
+            self.age = (self.age + 1) % self.window
+
 
     def update_values1_min_max(self, value):
-        if self.values1_min_index == -1 or self.values1_max_index == -1:
-            for i in range(0, len(self.values1)):
-                if self.values1[i] > self.values1_max_value:
-                    self.values1_max_value = self.values1[i]
-                    self.values1_max_index = i
-                if self.values1_min_value == 0 or self.values1[i] < self.values1_min_value:
-                    self.values1_min_value = self.values1[i]
-                    self.values1_min_index = i
-        elif value < self.values1_min_value:
+        #self.values2_min_value = min(self.values2)
+        #self.values2_max_value = max(self.values2)
+        #return
+
+        if self.values1_min_value == 0 or self.values1_max_value == 0:
+            self.values1_min_value = min(self.values1)
+            self.values1_max_value = max(self.values1)
+            self.values1_min_age = 0
+            self.values1_max_age = 0
+            return
+
+        if value <= self.values1_min_value:
             self.values1_min_value = value
-            self.values1_min_index = self.age
-        elif value > self.values1_max_value:
+            self.values1_min_age = 0
+            self.values1_max_age += 1
+        elif value >= self.values1_max_value:
             self.values1_max_value = value
-            self.values1_max_index = self.age
-        elif self.values1_min_index == self.age:
-            self.values1_min_value = 0
-            for i in range(0, len(self.values1)):
-                if self.values1_min_value == 0 or self.values1[i] < self.values1_min_value:
-                    self.values1_min_value = self.values1[i]
-                    self.values1_min_index = i
-        elif self.values1_max_index == self.age:
-            self.values1_max_value = 0
-            for i in range(0, len(self.values1)):
-                if self.values1[i] > self.values1_max_value:
-                    self.values1_max_value = self.values1[i]
-                    self.values1_max_index = i
+            self.values1_max_age = 0
+            self.values1_min_age += 1
+        else:
+            self.values1_min_age += 1
+            self.values1_max_age += 1
+
+        if self.values1_min_age >= self.window-1:
+            self.values1_min_value = min(self.values1)
+            self.values1_min_age += 1
+
+        if self.values1_max_age >= self.window-1:
+            self.values1_max_value = max(self.values1)
+            self.values1_max_age += 1
+
 
     def update_values2_min_max(self, value):
-        if self.values2_min_index == -1 or self.values2_max_index == -1:
-            for i in range(0, len(self.values2)):
-                if self.values2[i] > self.values2_max_value:
-                    self.values2_max_value = self.values2[i]
-                    self.values2_max_index = i
-                if self.values2_min_value == 0 or self.values2[i] < self.values2_min_value:
-                    self.values2_min_value = self.values2[i]
-                    self.values2_min_index = i
-        elif value < self.values2_min_value:
+        #self.values2_min_value = min(self.values2)
+        #self.values2_max_value = max(self.values2)
+        #return
+
+        if self.values2_min_value == 0 or self.values2_max_value == 0:
+            self.values2_min_value = min(self.values2)
+            self.values2_max_value = max(self.values2)
+            self.values2_min_age = 0
+            self.values2_max_age = 0
+            return
+
+        if value <= self.values2_min_value:
             self.values2_min_value = value
-            self.values2_min_index = self.age
-        elif value > self.values2_max_value:
+            self.values2_min_age = 0
+            self.values2_max_age += 1
+        elif value >= self.values2_max_value:
             self.values2_max_value = value
-            self.values2_max_index = self.age
-        elif self.values2_min_index == self.age:
-            self.values2_min_value = 0
-            for i in range(0, len(self.values2)):
-                if self.values2_min_value == 0 or self.values2[i] < self.values2_min_value:
-                    self.values2_min_value = self.values2[i]
-                    self.values2_min_index = i
-        elif self.values2_max_index == self.age:
-            self.values2_max_value = 0
-            for i in range(0, len(self.values2)):
-                if self.values2[i] > self.values2_max_value:
-                    self.values2_max_value = self.values2[i]
-                    self.values2_max_index = i
+            self.values2_max_age = 0
+            self.values2_min_age += 1
+        else:
+            self.values2_min_age += 1
+            self.values2_max_age += 1
+
+        if self.values2_min_age >= self.window-1:
+            self.values2_min_value = min(self.values2)
+            self.values2_min_age += 1
+
+        if self.values2_max_age >= self.window-1:
+            self.values2_max_value = max(self.values2)
+            self.values2_max_age += 1
+
 
     # detect if value1 crosses up over value2
     def crossup_detected(self, clear=True):
