07456346eb4df44c4e8d94bf0a7675f7b34d2a1d
Scott Glover <scottgl@gmail.com>
2018-12-03 17:01:08 -0600

    Attempting to improve performance in TimeSegmentValues class.

--- trader/lib/TimeSegmentValues.py
+++ trader/lib/TimeSegmentValues.py
@@ -12,6 +12,10 @@ class TimeSegmentValues(object):
         self.full = False
         self.value_smoother = value_smoother
         self.percent_smoother = percent_smoother
+        self.min_value = 0
+        self.min_value_index = -1
+        self.max_value = 0
+        self.max_value_index = -1
 
     def empty(self):
         if len(self.values) == 0:
@@ -27,24 +31,54 @@ class TimeSegmentValues(object):
             self.values.append(svalue)
             self.timestamps.append(ts)
         else:
-            self.values.append(value)
+            svalue = value
+            self.values.append(svalue)
             self.timestamps.append(ts)
 
         cnt = 0
         while (ts - self.timestamps[cnt]) > self.seconds_ts:
             cnt += 1
 
+        if self.ready():
+            if self.min_value_index == -1 or self.max_value_index == -1:
+                self.min_value_index = min(xrange(len(self.values)), key=self.values.__getitem__)
+                self.max_value_index = max(xrange(len(self.values)), key=self.values.__getitem__)
+
+                self.min_value = self.values[self.min_value_index]
+                self.max_value = self.values[self.max_value_index]
+            elif value <= self.min_value:
+                self.min_value = value
+                self.min_value_index = len(self.values) - 1
+            elif value >= self.max_value:
+                self.max_value = value
+                self.max_value_index = len(self.values) - 1
+
         if cnt != 0:
+            self.min_value_index -= cnt
+            self.max_value_index -= cnt
+            if self.min_value_index <= 0:
+                self.min_value_index = min(xrange(len(self.values)), key=self.values.__getitem__)
+                self.min_value = self.values[self.min_value_index]
+            if self.max_value_index <= 0:
+                self.max_value_index = max(xrange(len(self.values)), key=self.values.__getitem__)
+                self.max_value = self.values[self.max_value_index]
             self.timestamps = self.timestamps[cnt:]
             self.values = self.values[cnt:]
             if not self.full:
                 self.full = True
 
+
     def min(self):
+        if not self.ready():
+            return 0
         return min(self.values)
+        #return self.min_value
 
     def max(self):
+        if not self.ready():
+            return 0
         return max(self.values)
+        #return self.max_value
 
     def get_values(self):
         return self.values
@@ -53,22 +87,22 @@ class TimeSegmentValues(object):
         return len(self.values)
 
     def first_value(self):
-        if self.empty():
+        if not self.ready():
             return 0
         return self.values[0]
 
     def last_value(self):
-        if self.empty():
+        if not self.ready():
             return 0
         return self.values[-1]
 
     def diff(self):
-        if self.empty():
+        if not self.ready():
             return 0
         return self.values[-1] - self.values[0]
 
     def percent_change(self):
-        if self.empty():
+        if not self.ready():
             return None
         value1 = self.first_value()
         value2 = self.last_value()
