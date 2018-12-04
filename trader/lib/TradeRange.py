4f86e7ea2fc69f84b0ee09d45c3e5de9f3108468
Scott Glover <scottgl@gmail.com>
2018-12-04 14:05:31 -0600

    Updates to TradeRange class.

--- trader/lib/TradeRange.py
+++ trader/lib/TradeRange.py
@@ -3,46 +3,25 @@
 # price crossovers through the range. One way to do this is to use EMA which
 # should always be in the center of the trading range, and an EMA which closely
 # follows the price
-from trader.indicator.EMA import EMA
-from trader.myhelpers import *
-import logging
-
-logger = logging.getLogger(__name__)
-
 # kline format: [ time, low, high, open, close, volume ]
+from trader.indicator.EMA import EMA
+
 
 class TradeRange(object):
-    def __init__(self, accnt=None, ticker_id=None):
+    def __init__(self, ticker_id=None, tick_size=0.01):
         self.peaks = []
         self.valleys = []
         self.ema_prices= []
         self.ema = EMA(8)
+        self.tick_size = tick_size
         self.major_resistance = 0.0
         self.major_support = 0.0
         self.active_resistance = 0.0
         self.active_support = 0.0
-        self.accnt = accnt
         self.ticker_id = ticker_id
 
     def update(self, price):
         self.ema_prices.append(self.ema.update(price))
-
-    def get_prices_24hr(self):
-        prices = []
-        for kline in retrieve_klines_days(self.ticker_id, days=1):
-            prices.append(float(kline[3]))
-            prices.append(float(kline[4]))
-
-        return prices
-
-    def get_prices_4hr(self):
-        prices= []
-
-        for kline in retrieve_klines_last_hour(self.ticker_id, hours=4):
-            prices.append(float(kline[3]))
-            prices.append(float(kline[4]))
-
-        return prices
 
     # create dictionary where price_crossovers[price] = cross count
     def get_price_crossover_counts(self, prices):
@@ -64,8 +43,6 @@ class TradeRange(object):
         while current_price <= max_prices:
             cross_count = 0
             counter += 1.0
-
-            #print(int(100.0 * current_price / max_prices), current_price)
 
             if current_price in prices_completed:#current_price not in prices or current_price in prices_completed:
                 current_price = round(current_price + step_size, round_size)
@@ -106,12 +83,7 @@ class TradeRange(object):
     # return dictionary for which each price (key) is a list of all
     # indices (value) in prices that have that price value
     def get_indices_by_price(self, prices):
-        if self.ticker_id.endswith('USD'):
-            step_size = 0.01
-        else:
-            step_size = 0.00001
-
-        round_size = '{:f}'.format(step_size).index('1') - 1
+        round_size = '{:f}'.format(self.tick_size).index('1') - 1
         print(round_size)
         current_price = self.major_support
         price_indices = {}
@@ -123,7 +95,7 @@ class TradeRange(object):
                     indices.append(i)
             if len(indices) > 2:
                 price_indices[current_price] = indices
-            current_price = round(current_price + step_size, round_size)
+            current_price = round(current_price + self.tick_size, round_size)
         return price_indices
 
     def get_largest_indices_by_price(self, prices):
