adbe869137dfcf8ba95bda8adabd2e905689dab4
Scott Glover <scottgl@gmail.com>
2018-12-03 21:44:26 -0600

    Added script ubuntu_prereq.sh

---
+++ ubuntu_prereq.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+apt-get update
+apt-get install -y python-pip python-autobahn python-flask python-twisted python-tz python-numpy python-six 
+apt-get install -y python-requests python-matplotlib python-scipy python-sklearn python-pymongo python-aniso8601
+apt-get install -y python-websocket cython python-pandas python-psycopg2 python-cryptography python-gevent
+pip install --upgrade pip
+pip install dateparser
+pip install -r requirements.txt
