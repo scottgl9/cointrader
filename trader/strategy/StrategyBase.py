af2e5696add1fb3a2ece94c27277e0ec5898509b
Scott Glover <scottgl@gmail.com>
2018-12-23 18:22:07 -0600

    Removed more outdated signals.

--- trader/strategy/StrategyBase.py
+++ trader/strategy/StrategyBase.py
@@ -3,23 +3,18 @@ from datetime import datetime
 from trader.lib.MessageHandler import Message, MessageHandler
 from trader.signal.SignalHandler import SignalHandler
 
-from trader.signal.Bollinger_Bands_Signal import Bollinger_Bands_Signal
 from trader.signal.long.Currency_Long_EMA import Currency_EMA_Long
 from trader.signal.EFI_Breakout_Signal import EFI_Breakout_Signal
 from trader.signal.EMA_OBV_Crossover import EMA_OBV_Crossover
 from trader.signal.Hybrid_Crossover import Hybrid_Crossover
-from trader.signal.Hybrid2_Crossover import Hybrid2_Crossover
 from trader.signal.Hybrid_Crossover_MM import Hybrid_Crossover_MM
 from trader.signal.Hybrid_Crossover_Test import Hybrid_Crossover_Test
 from trader.signal.KST_Crossover import KST_Crossover
 from trader.signal.MACD_Crossover import MACD_Crossover
 from trader.signal.PPO_OBV import PPO_OBV
-from trader.signal.price_channel_signal import price_channel_signal
 from trader.signal.PMO_Crossover import PMO_Crossover
-from trader.signal.RSI_OBV import RSI_OBV
 from trader.signal.SignalHandler import SignalHandler
 from trader.signal.SigType import SigType
-from trader.signal.Slope_Crossover import Slope_Crossover
 from trader.signal.TD_Sequential_Signal import TD_Sequential_Signal
 from trader.signal.TSI_Signal import TSI_Signal
 from trader.signal.TSV_Signal import TSV_Signal
@@ -51,22 +46,17 @@ class StrategyBase(object):
 
     @staticmethod
     def select_signal_name(name, accnt=None, symbol=None):
-        if name =="Bollinger_Bands_Signal": return Bollinger_Bands_Signal(accnt, symbol)
-        elif name == "BTC_USDT_Signal": return BTC_USDT_Signal(accnt)
+        if name == "BTC_USDT_Signal": return BTC_USDT_Signal(accnt)
         elif name == "Currency_Long_EMA": return Currency_EMA_Long(accnt)
         elif name == "EFI_Breakout_Signal": return EFI_Breakout_Signal(accnt, symbol)
         elif name == "EMA_OBV_Crossover": return EMA_OBV_Crossover(accnt, symbol)
         elif name == "Hybrid_Crossover": return Hybrid_Crossover(accnt, symbol)
-        elif name == "Hybrid2_Crossover": return Hybrid2_Crossover(accnt, symbol)
         elif name == "Hybrid_Crossover_MM": return Hybrid_Crossover_MM(accnt, symbol)
         elif name == "Hybrid_Crossover_Test": return Hybrid_Crossover_Test(accnt, symbol)
         elif name == "KST_Crossover": return KST_Crossover(accnt, symbol)
         elif name == "MACD_Crossover": return MACD_Crossover(accnt, symbol)
         elif name == "PPO_OBV": return PPO_OBV(accnt, symbol)
-        elif name == "price_channel_signal": return price_channel_signal(accnt, symbol)
         elif name == "PMO_Crossover": return PMO_Crossover(accnt, symbol)
-        elif name == "RSI_OBV": return RSI_OBV(accnt, symbol)
-        elif name == "Slope_Crossover": return Slope_Crossover(accnt, symbol)
         elif name == "TD_Sequential_Signal": return TD_Sequential_Signal(accnt, symbol)
         elif name == "TSI_Signal": return TSI_Signal(accnt, symbol)
         elif name == "TSV_Signal": return TSV_Signal(accnt, symbol)
