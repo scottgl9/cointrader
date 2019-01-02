076baa781cd7ac89783a172b7af03a9b783fd9d4
Scott Glover <scottgl@gmail.com>
2019-01-02 16:12:04 -0600

    Added misc directory, and run_simulate.sh script.

---
+++ misc/run_simulate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+tools/binance_simulate.py -f ./cryptocurrency_database.miniticker_collection_04032018.db
+tools/binance_simulate.py -f ./cryptocurrency_database.miniticker_collection_04092018.db
+tools/binance_simulate.py -f ./cryptocurrency_database.miniticker_collection_05312018.db
+tools/binance_simulate.py -f ./cryptocurrency_database.miniticker_collection_11202018.db
+tools/binance_simulate.py -f ./cryptocurrency_database.miniticker_collection_11232018.db
+
