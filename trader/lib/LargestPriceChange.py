e15692969a01b5978de4880eccb27a161b65a658
Scott Glover <scottgl@gmail.com>
2019-01-21 16:32:45 -0600

    Updates to LargestPriceChange.

--- trader/lib/LargestPriceChange.py
+++ trader/lib/LargestPriceChange.py
@@ -5,19 +5,21 @@ class LargestPriceChange(object):
         self.root = PriceSegment(self.prices, self.timestamps)
 
     def divide_price_segments(self):
-        pass
+        self.root.split()
 
 
 class SplitPriceSegment(object):
-    def __init__(self, prices=None, timestamps=None):
+    def __init__(self, prices=None, timestamps=None, min_percent_price=1.0, min_segment_size=50):
         self.prices = prices
         self.timestamps = timestamps
         self.min_price = 0
-        self.min_price_index = 0
+        self.min_price_index = -1
         self.min_price_ts = 0
         self.max_price = 0
-        self.max_price_index = 0
+        self.max_price_index = -1
         self.max_price_ts = 0
+        self.min_percent_price = min_percent_price
+        self.min_segment_size = min_segment_size
         self.start_segment = None           # start PriceSegment
         self.mid_segment = None             # mid PriceSegment
         self.end_segment = None             # end PriceSegment
@@ -29,6 +31,25 @@ class SplitPriceSegment(object):
         self.min_price = min(self.prices)
         self.min_price_index = self.prices.index(self.min_price)
         self.min_price_ts = self.timestamps[self.min_price_index]
+
+        if 100.0*(self.max_price - self.min_price) / self.min_price <= self.min_percent_price:
+            return
+
+        if abs(self.max_price_index - self.min_price_index) < self.min_segment_size:
+            return
+
+        if self.min_price_index < self.max_price_index:
+            if self.min_price_index < self.min_segment_size:
+                return
+            if (len(self.prices) - self.max_price_index) < self.min_segment_size:
+                return
+        elif self.min_price_index > self.max_price_index:
+            if self.max_price_index < self.min_segment_size:
+                return
+            if (len(self.prices) - self.min_price_index) < self.min_segment_size:
+                return
+        else:
+            return
 
         if self.max_price_ts < self.min_price_ts:
             start_price_values = self.prices[0:(self.max_price_index - 1)]
