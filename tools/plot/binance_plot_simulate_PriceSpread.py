ff4d3fa352823570a0f82323d63b21ccdde8fd30
Scott Glover <scottgl@gmail.com>
2019-02-02 08:13:16 -0600

    Added PriceSpread class and plotter for class.

---
+++ tools/plot/binance_plot_simulate_PriceSpread.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python
+
+import sys
+try:
+    import trader
+except ImportError:
+    sys.path.append('.')
+    import trader
+
+import sqlite3
+import sys
+import os
+from trader.account.binance.client import Client
+from trader.config import *
+import matplotlib.pyplot as plt
+import argparse
+from trader.indicator.OBV import OBV
+from trader.indicator.EMA import EMA
+from trader.lib.PriceSpread import PriceSpread
+
+def get_rows_as_msgs(c):
+    msgs = []
+    for row in c:
+        msg = {'E': row[0], 'c': row[1], 'h': row[2], 'l': row[3],
+               'o': row[4], 'q': row[5], 's': row[6], 'v': row[7]}
+        msgs.append(msg)
+    return msgs
+
+
+def simulate(conn, client, base, currency, type="channel"):
+    ticker_id = "{}{}".format(base, currency)
+    c = conn.cursor()
+    #c.execute("SELECT * FROM miniticker WHERE s='{}' ORDER BY E ASC".format(ticker_id))
+    c.execute("SELECT E,c,h,l,o,q,s,v FROM miniticker WHERE s='{}'".format(ticker_id)) # ORDER BY E ASC")")
+
+    price_spread = PriceSpread()
+    obv = OBV()
+    obv_ema12 = EMA(12, scale=24) #EMA(12, scale=24)
+    obv_ema26 = EMA(26, scale=24) #EMA(26, scale=24)
+    obv_ema50 = EMA(50,scale=24) #EMA(50, scale=24, lag_window=5)
+    obv_ema12_values = []
+    obv_ema26_values = []
+    obv_ema50_values = []
+    close_prices = []
+    open_prices = []
+    low_prices = []
+    high_prices = []
+    volumes = []
+    price_spread_asks = []
+    price_spread_bids = []
+
+    i=0
+    for msg in get_rows_as_msgs(c):
+        close = float(msg['c'])
+        low = float(msg['l'])
+        high = float(msg['h'])
+        open = float(msg['o'])
+        volume = float(msg['v'])
+        ts = int(msg['E'])
+
+        obv_value = obv.update(close=close, volume=volume)
+        obv_ema12_values.append(obv_ema12.update(obv_value))
+        obv_ema26_values.append(obv_ema26.update(obv_value))
+        obv_ema50_values.append(obv_ema50.update(obv_value))
+
+        price_spread.update(close, ts)
+
+        if price_spread.bid_price != 0:
+            price_spread_bids.append(price_spread.bid_price)
+        if price_spread.ask_price != 0:
+            price_spread_asks.append(price_spread.ask_price)
+
+        close_prices.append(close)
+        open_prices.append(open)
+        low_prices.append(low)
+        high_prices.append(high)
+        i += 1
+
+    plt.subplot(211)
+    #symprice, = plt.plot(close_prices, label=ticker_id)
+    plt.plot(price_spread_bids)
+    plt.plot(price_spread_asks)
+    #plt.legend(handles=[symprice])
+    plt.subplot(212)
+    fig21, = plt.plot(obv_ema12_values, label='OBV12')
+    fig22, = plt.plot(obv_ema26_values, label='OBV26')
+    fig23, = plt.plot(obv_ema50_values, label='OBV50')
+    plt.legend(handles=[fig21, fig22, fig23])
+    #plt.plot(signal_values)
+    #plt.plot(tsi_values)
+    plt.show()
+
+if __name__ == '__main__':
+    client = None
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', action='store', dest='filename',
+                        default='cryptocurrency_database.miniticker_collection_04092018.db',
+                        help='filename of kline sqlite db')
+
+    parser.add_argument('-b', action='store', dest='base',
+                        default='BTC',
+                        help='base part of symbol')
+
+    parser.add_argument('-c', action='store', dest='currency',
+                        default='USDT',
+                        help='currency part of symbol')
+
+    results = parser.parse_args()
+
+    if not os.path.exists(results.filename):
+        print("file {} doesn't exist, exiting...".format(results.filename))
+        sys.exit(-1)
+
+    filename = results.filename
+    base = results.base
+    currency = results.currency
+
+    print("Loading {}".format(filename))
+    conn = sqlite3.connect(filename)
+
+    simulate(conn, client, base, currency, type="MACD")
+    conn.close()
---
+++ trader/lib/PriceSpread.py
@@ -0,0 +1,26 @@
+# Break down the moving close price into two classifications: ask or bid
+from trader.indicator.SMA import SMA
+
+
+class PriceSpread(object):
+    def __init__(self):
+        self.sma = SMA(10)
+        self.sma_ask = SMA(10)
+        self.sma_bid = SMA(10)
+        self.ask_price = 0
+        self.bid_price = 0
+        self.last_ask_price = 0
+        self.last_bid_price = 0
+
+    def update(self, price, ts):
+        self.sma.update(price)
+        if float(price) > self.sma.result:
+            if float(price) != self.last_bid_price and float(price) != self.bid_price:
+                self.last_ask_price = self.ask_price
+                self.ask_price = self.sma_ask.update((float(price)))
+        elif float(price) < self.sma.result:
+            if float(price) != self.last_ask_price and float(price) != self.ask_price:
+                self.last_bid_price = self.bid_price
+                self.bid_price = self.sma_bid.update(float(price))
+
+        return self.bid_price, self.ask_price
