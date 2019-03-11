4fcb74cfc53947d6178c475252ea586180c0637b
Scott Glover <scottgl@gmail.com>
2019-03-10 21:25:53 -0500

    Updated PriceSegmentTree.

--- trader/lib/PriceSegmentTree.py
+++ trader/lib/PriceSegmentTree.py
@@ -147,7 +147,7 @@ class PriceSegmentNode(object):
         return self.percent
 
     # recursively split prices/timestamps to create tree with start_segment, mid_segment, and end_segment
-    def split(self, prices, timestamps, n=0, parent=None):
+    def split(self, prices, timestamps, n=0, t=0, parent=None):
         self.start_price = prices[0]
         self.end_price = prices[-1]
         self.start_ts = timestamps[0]
@@ -155,12 +155,16 @@ class PriceSegmentNode(object):
         self.depth = n
         self.parent = parent
 
+        # if parent was split into 3, we don't need to calculate these values
+        #if parent and not parent.half_split:
         self.max_price = max(prices)
         self.max_price_index = prices.index(self.max_price)
         self.max_price_ts = timestamps[self.max_price_index]
         self.min_price = min(prices)
         self.min_price_index = prices.index(self.min_price)
         self.min_price_ts = timestamps[self.min_price_index]
+        #else:
+        #    self.half_split = True
 
         #diff_secs = (timestamps[-1] - timestamps[0])
         #if diff_secs <= self.min_segment_seconds * 1000 * 3:
@@ -203,11 +207,11 @@ class PriceSegmentNode(object):
 
             start_price_values = prices[0:mid_index]
             start_ts_values = timestamps[0:mid_index]
-            self.start_segment.split(start_price_values, start_ts_values, n+1, parent=self)
+            self.start_segment.split(start_price_values, start_ts_values, n+1, 1, parent=self)
 
             end_price_values = prices[mid_index:-1]
             end_ts_values = timestamps[mid_index:-1]
-            self.end_segment.split(end_price_values, end_ts_values, n+1, parent=self)
+            self.end_segment.split(end_price_values, end_ts_values, n+1, 3, parent=self)
         else:
             # split prices and timestamps into three parts
             if self.max_price_ts < self.min_price_ts:
@@ -225,14 +229,14 @@ class PriceSegmentNode(object):
 
             start_price_values = prices[0: (index1 - 1)]
             start_ts_values = timestamps[0: (index1 - 1)]
-            self.start_segment.split(start_price_values, start_ts_values, n+1, parent=self)
+            self.start_segment.split(start_price_values, start_ts_values, n+1, 1, parent=self)
 
             mid_price_values = prices[index1: index2]
             mid_ts_values = timestamps[index1: index2]
-            self.mid_segment.split(mid_price_values, mid_ts_values, n+1, parent=self)
+            self.mid_segment.split(mid_price_values, mid_ts_values, n+1, 2, parent=self)
 
             end_price_values = prices[(index2 + 1):-1]
             end_ts_values = timestamps[(index2 + 1):-1]
-            self.end_segment.split(end_price_values, end_ts_values, n+1, parent=self)
+            self.end_segment.split(end_price_values, end_ts_values, n+1, 3, parent=self)
 
         return True
