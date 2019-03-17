17f1a40ffb5c54e7d63fac4f9435fdcad41a3980
Scott Glover <scottgl@gmail.com>
2019-03-17 14:14:54 -0500

    Reorganized native ValueLag and native Crossover classes.

--- trader/lib/native/Crossover.c
+++ trader/lib/native/Crossover.c
@@ -1,51 +1,8 @@
-#include <Python.h>
-#include <structmember.h>
-
-#define BOOL int
-#define TRUE 1
-#define FALSE 0
-
-typedef struct {
-    PyObject_HEAD
-    /* internal data. */
-    int pre_window;
-    int window;
-    int pre_age;
-    int age;
-    int last_age;
-
-    double values1_min_value;
-    int values1_min_age;
-    double values1_max_value;
-    int values1_max_age;
-
-    double values2_min_value;
-    int values2_min_age;
-    double values2_max_value;
-    int values2_max_age;
-
-    PyListObject *values1;
-    PyListObject *values2;
-    PyListObject *pre_values1;
-    PyListObject *pre_values2;
-
-    BOOL values_under;
-    BOOL values_over;
-    BOOL crossup;
-    BOOL crossdown;
-} Crossover;
+#include "Crossover.h"
 
 static void
 Crossover_dealloc(Crossover* self)
 {
-//    if (self->values1)
-//        self->ob_type->tp_free(self->values1);
-//    if (self->values2)
-//        self->ob_type->tp_free(self->values2);
-//    if (self->pre_values1)
-//        self->ob_type->tp_free(self->pre_values1);
-//    if (self->pre_values2)
-//        self->ob_type->tp_free(self->pre_values2);
     Py_XDECREF(self->pre_values2);
     Py_XDECREF(self->pre_values1);
     Py_XDECREF(self->values2);
@@ -99,31 +56,6 @@ Crossover_init(Crossover *self, PyObject *args, PyObject *kwds)
 
     return 0;
 }
-
-static PyMemberDef Crossover_members[] = {
-    {"pre_window", T_INT, offsetof(Crossover, pre_window), 0, "crossoverobj pre_window"},
-    {"window", T_INT, offsetof(Crossover, window), 0, "crossoverobj window"},
-    {"age", T_INT, offsetof(Crossover, age), 0, "crossoverobj age"},
-    {"last_age", T_INT, offsetof(Crossover, last_age), 0, "crossoverobj last_age"},
-    {"pre_age", T_INT, offsetof(Crossover, pre_age), 0, "crossoverobj pre_age"},
-    {"values_under", T_INT, offsetof(Crossover, values_under), 0, "crossoverobj values_under"},
-    {"values_over", T_INT, offsetof(Crossover, values_over), 0, "crossoverobj values_over"},
-    {"crossup", T_INT, offsetof(Crossover, crossup), 0, "crossoverobj crossup"},
-    {"crossdown", T_INT, offsetof(Crossover, crossdown), 0, "crossoverobj crossdown"},
-    {"values1", T_OBJECT_EX, offsetof(Crossover, values1), 0, "crossoverobj values1"},
-    {"values2", T_OBJECT_EX, offsetof(Crossover, values2), 0, "crossoverobj values2"},
-    {"pre_values1", T_OBJECT_EX, offsetof(Crossover, pre_values1), 0, "crossoverobj pre_values1"},
-    {"pre_values2", T_OBJECT_EX, offsetof(Crossover, pre_values2), 0, "crossoverobj pre_values2"},
-    {"values1_min_value", T_DOUBLE, offsetof(Crossover, values1_min_value), 0, "crossoverobj values1_min_value"},
-    {"values1_min_age", T_INT, offsetof(Crossover, values1_min_age), 0, "crossoverobj values1_min_age"},
-    {"values1_max_value", T_DOUBLE, offsetof(Crossover, values1_max_value), 0, "crossoverobj values1_max_value"},
-    {"values1_max_age", T_INT, offsetof(Crossover, values1_max_age), 0, "crossoverobj values1_max_age"},
-    {"values2_min_value", T_DOUBLE, offsetof(Crossover, values2_min_value), 0, "crossoverobj values2_min_value"},
-    {"values2_min_age", T_INT, offsetof(Crossover, values2_min_age), 0, "crossoverobj values2_min_age"},
-    {"values2_max_value", T_DOUBLE, offsetof(Crossover, values2_max_value), 0, "crossoverobj values2_max_value"},
-    {"values2_max_age", T_INT, offsetof(Crossover, values2_max_age), 0, "crossoverobj values2_max_age"},
-    {NULL}  /* Sentinel */
-};
 
 static double get_min(PyListObject *values)
 {
@@ -342,68 +274,6 @@ static PyObject *Crossover_crossdown_detected(Crossover* self, PyObject *args)
     return Py_False;
 }
 
-static PyMethodDef Crossover_methods[] = {
-    {"update", (PyCFunction)Crossover_update, METH_VARARGS|METH_KEYWORDS,
-     "Update Crossover",
-    },
-    {"crossup_detected", (PyCFunction)Crossover_crossup_detected, METH_NOARGS,
-     "Crossover crossup detected",
-    },
-    {"crossdown_detected", (PyCFunction)Crossover_crossdown_detected, METH_NOARGS,
-     "Crossover crossdown detected",
-    },
-    {NULL}  /* Sentinel */
-};
-
-static PyTypeObject Crossover_MyTestType = {
-    PyObject_HEAD_INIT(NULL)
-    0,                         /*ob_size*/
-    "Crossover.Crossover",                 /*tp_name*/
-    sizeof(Crossover),               /*tp_basicsize*/
-    0,                         /*tp_itemsize*/
-    (destructor)Crossover_dealloc,   /*tp_dealloc*/
-    0,                         /*tp_print*/
-    0,                         /*tp_getattr*/
-    0,                         /*tp_setattr*/
-    0,                         /*tp_compare*/
-    0,                         /*tp_repr*/
-    0,                         /*tp_as_number*/
-    0,                         /*tp_as_sequence*/
-    0,                         /*tp_as_mapping*/
-    0,                         /*tp_hash */
-    0,                         /*tp_call*/
-    0,                         /*tp_str*/
-    0,                         /*tp_getattro*/
-    0,                         /*tp_setattro*/
-    0,                         /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,/*tp_flags*/
-    "Crossover objects",             /* tp_doc */
-    0,                         /* tp_traverse */
-    0,                         /* tp_clear */
-    0,                         /* tp_richcompare */
-    0,                         /* tp_weaklistoffset */
-    0,                         /* tp_iter */
-    0,                         /* tp_iternext */
-    Crossover_methods,               /* tp_methods */
-    Crossover_members,               /* tp_members */
-    0,                         /* tp_getset */
-    0,                         /* tp_base */
-    0,                         /* tp_dict */
-    0,                         /* tp_descr_get */
-    0,                         /* tp_descr_set */
-    0,                         /* tp_dictoffset */
-    (initproc)Crossover_init,        /* tp_init */
-    0,                         /* tp_alloc */
-    Crossover_new,                   /* tp_new */
-};
-
-//static PyMethodDef Crossover_methods[] = {
-//    {NULL}  /* Sentinel */
-//};
-
-#ifndef PyMODINIT_FUNC  /* declarations for DLL import/export */
-#define PyMODINIT_FUNC void
-#endif
 PyMODINIT_FUNC
 initCrossover(void)
 {
--- trader/lib/native/ValueLag.c
+++ trader/lib/native/ValueLag.c
@@ -1,14 +1,4 @@
-#include <Python.h>
-#include <structmember.h>
-
-typedef struct {
-    PyObject_HEAD
-    /* internal data. */
-    int window;
-    int age;
-    double result;
-    PyListObject *values;
-} ValueLag;
+#include "ValueLag.h"
 
 static void
 ValueLag_dealloc(ValueLag* self)
@@ -41,14 +31,6 @@ ValueLag_init(ValueLag *self, PyObject *args, PyObject *kwds)
 
     return 0;
 }
-
-static PyMemberDef ValueLag_members[] = {
-    {"window", T_INT, offsetof(ValueLag, window), 0, "valuelagobj window"},
-    {"age", T_INT, offsetof(ValueLag, age), 0, "valuelagobj age"},
-    {"result", T_DOUBLE, offsetof(ValueLag, result), 0, "valuelagobj result"},
-    {"values", T_OBJECT, offsetof(ValueLag, values), 0, "valuelagobj values"},
-    {NULL}  /* Sentinel */
-};
 
 static PyObject *
 ValueLag_update(ValueLag* self, PyObject *args)
@@ -90,65 +72,7 @@ static PyObject *ValueLag_ready(ValueLag* self, PyObject *args)
     return Py_False;
 }
 
-static PyMethodDef ValueLag_methods[] = {
-    {"update", (PyCFunction)ValueLag_update, METH_VARARGS,
-     "Update ValueLag",
-    },
-    {"ready", (PyCFunction)ValueLag_ready, METH_VARARGS,
-     "Ready ValueLag",
-    },
-    {NULL}  /* Sentinel */
-};
 
-static PyTypeObject ValueLag_MyTestType = {
-    PyObject_HEAD_INIT(NULL)
-    0,                         /*ob_size*/
-    "ValueLag.ValueLag",                 /*tp_name*/
-    sizeof(ValueLag),               /*tp_basicsize*/
-    0,                         /*tp_itemsize*/
-    (destructor)ValueLag_dealloc,   /*tp_dealloc*/
-    0,                         /*tp_print*/
-    0,                         /*tp_getattr*/
-    0,                         /*tp_setattr*/
-    0,                         /*tp_compare*/
-    0,                         /*tp_repr*/
-    0,                         /*tp_as_number*/
-    0,                         /*tp_as_sequence*/
-    0,                         /*tp_as_mapping*/
-    0,                         /*tp_hash */
-    0,                         /*tp_call*/
-    0,                         /*tp_str*/
-    0,                         /*tp_getattro*/
-    0,                         /*tp_setattro*/
-    0,                         /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,/*tp_flags*/
-    "ValueLag objects",             /* tp_doc */
-    0,                         /* tp_traverse */
-    0,                         /* tp_clear */
-    0,                         /* tp_richcompare */
-    0,                         /* tp_weaklistoffset */
-    0,                         /* tp_iter */
-    0,                         /* tp_iternext */
-    ValueLag_methods,               /* tp_methods */
-    ValueLag_members,               /* tp_members */
-    0,                         /* tp_getset */
-    0,                         /* tp_base */
-    0,                         /* tp_dict */
-    0,                         /* tp_descr_get */
-    0,                         /* tp_descr_set */
-    0,                         /* tp_dictoffset */
-    (initproc)ValueLag_init,        /* tp_init */
-    0,                         /* tp_alloc */
-    ValueLag_new,                   /* tp_new */
-};
-
-//static PyMethodDef ValueLag_methods[] = {
-//    {NULL}  /* Sentinel */
-//};
-
-#ifndef PyMODINIT_FUNC  /* declarations for DLL import/export */
-#define PyMODINIT_FUNC void
-#endif
 PyMODINIT_FUNC
 initValueLag(void)
 {
