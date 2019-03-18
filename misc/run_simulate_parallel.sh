ebb2cf88ac0f933a04b6c9a8d12c127c153d0d7d
Scott Glover <scottgl@gmail.com>
2019-03-18 15:25:51 -0500

    Updated cache.

--- misc/run_simulate_parallel.sh
+++ misc/run_simulate_parallel.sh
@@ -6,4 +6,5 @@ DB_LIST+=" cryptocurrency_database.miniticker_collection_11202018.db"
 DB_LIST+=" cryptocurrency_database.miniticker_collection_11232018.db"
 DB_LIST+=" cryptocurrency_database.miniticker_collection_01192019.db"
 DB_LIST+=" cryptocurrency_database.miniticker_collection_01202019.db"
+DB_LIST+=" cryptocurrency_database.miniticker_collection_03142019.db"
 echo $DB_LIST | tr ' ' '\n' | parallel -j 4 python tools/binance_simulate.py -f {}
