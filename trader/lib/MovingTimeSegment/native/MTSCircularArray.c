bd062f70b79298df1396c06f1b26cbfcb3f660f8
Scott Glover <scottgl@gmail.com>
2019-03-25 10:13:03 -0500

    Updates to native MTSCircularArray.

--- trader/lib/MovingTimeSegment/native/MTSCircularArray.c
+++ trader/lib/MovingTimeSegment/native/MTSCircularArray.c
@@ -63,6 +63,84 @@ MTSCircularArray_add(MTSCircularArray* self, PyObject *args, PyObject *kwds)
                                      &value, &ts))
         return NULL;
 
+    if (self->minmax) {
+        if (self->min_value == 0 || value < self->min_value) {
+            self->min_value = value;
+            self->min_age = 0;
+        } else {
+            self->min_age += 1;
+        }
+
+        if (value > self->max_value) {
+            self->max_value = value;
+            self->max_age = 0;
+        } else {
+            self->max_age += 1;
+        }
+    }
+
+    //self._values[int(self.age)] = value;
+    //self._timestamps[int(self.age)] = ts;
+    PyList_SetItem((PyObject *)self->values, self->age, Py_BuildValue("d", value));
+    PyList_SetItem((PyObject *)self->timestamps, self->age, Py_BuildValue("l", ts));
+
+    // if start_age back at index 0, and diff in timestamps >= win_sec_ts,
+    // then resize current_size of circular array
+
+    if (self->start_age == 0) {
+        double ts0 = PyInt_AsLong(PyList_GetItem((PyObject *)self->timestamps, 0));
+        if ((ts - ts0) >= self->win_secs_ts) {
+            self->current_size = self->age;
+        }
+    }
+
+    // index of most recent value and ts
+    self->end_age = self->age;
+
+    if (self->current_size != 0) {
+        self->start_age = (self->start_age + 1) % self->current_size;
+        self->age = (self->age + 1) % self->current_size;
+    } else {
+        self->age = (self->age + 1) % self->max_win_size;
+    }
+
+    if (self->minmax && self->current_size != 0) {
+        if (self->min_age > self->current_size) {
+            double cur_value = 0;
+            int min_age=0;
+            double min_value = 0;
+            int age = self->start_age;
+
+            for (int i=0; i<self->current_size; i++) {
+                cur_value = PyFloat_AS_DOUBLE(PyList_GetItem((PyObject *)self->values, age));
+                if (min_value == 0 || cur_value < min_value) {
+                    min_value = cur_value;
+                    min_age = age;
+                }
+                age = (age + 1) % self->current_size;
+            }
+            self->min_value = min_value;
+            self->min_age = min_age;
+        }
+
+        if (self->max_age > self->current_size) {
+            double cur_value = 0;
+            int max_age=0;
+            double max_value = 0;
+            int age = self->start_age;
+
+            for (int i=0; i<self->current_size; i++) {
+                cur_value = PyFloat_AS_DOUBLE(PyList_GetItem((PyObject *)self->values, age));
+                if (max_age < cur_value) {
+                    max_value = cur_value;
+                    max_age = age;
+                }
+                age = (age + 1) % self->current_size;
+            }
+            self->max_value = max_value;
+            self->max_age = age;
+        }
+    }
 }
 
 static PyObject *MTSCircularArray_ready(MTSCircularArray* self, PyObject *args)
