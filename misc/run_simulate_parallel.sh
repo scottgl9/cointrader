eaff100c53d37ab3b84faf2b1ab102cf6e8557b9
Scott Glover <scottgl@gmail.com>
2019-03-29 21:57:14 -0500

    Updated cache.

--- misc/run_simulate_parallel.sh
+++ misc/run_simulate_parallel.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 signal_name="Hybrid_Crossover_Test"
+strategy_name="basic_signal_market_strategy"
 if [ "$#" -eq 1 ]; then
 	signal_name=$1
+elif [ "$#" -eq 2 ]; then
+	signal_name=$1
+	strategy_name=$2
 fi
 
 echo "Using signal $signal_name"
@@ -14,4 +18,4 @@ DB_LIST+=" cryptocurrency_database.miniticker_collection_11232018.db"
 DB_LIST+=" cryptocurrency_database.miniticker_collection_01192019.db"
 DB_LIST+=" cryptocurrency_database.miniticker_collection_01202019.db"
 DB_LIST+=" cryptocurrency_database.miniticker_collection_03142019.db"
-echo $DB_LIST | tr ' ' '\n' | parallel -j 4 python tools/binance_simulate.py -f {} -g $signal_name
+echo $DB_LIST | tr ' ' '\n' | parallel -j 4 python tools/binance_simulate.py -f {} -s $strategy_name -g $signal_name
