313770ee38319cb0de80a7de4f30899d77069a8f
Scott Glover <scottgl@gmail.com>
2019-04-10 14:52:27 -0500

    Updates to LSMA indicator, also added plotter for LSMA.

--- trader/indicator/LSMA.py
+++ trader/indicator/LSMA.py
@@ -8,6 +8,7 @@ class LSMA(object):
         self.timestamps = []
         self.values = []
         self.age = 0
+        self.start_ts = 0
         self._sumx = 0
         self._sumx2 = 0
         self._sumxy = 0
@@ -16,19 +17,27 @@ class LSMA(object):
         self.m = 0
         self.b = 0
         self.r = 0
+        self.n = 0
+        self.result = 0
 
     def ready(self):
         return len(self.values) == self.window
 
     def update(self, value, ts):
+        if not self.start_ts:
+            self.start_ts = ts
+        #ts = float(ts - self.start_ts) / 1000.0
+
         if len(self.values) < self.window:
             self.values.append(float(value))
             self.timestamps.append(ts)
-            self._sumx += float(ts)
-            self._sumx2 += float(ts) * float(ts)
-            self._sumxy += float(ts) * float(value)
-            self._sumy += float(value)
-            self._sumy2 += float(value) * float(value)
+            self._sumx += ts
+            self._sumx2 += ts * ts
+            self._sumxy += ts * value
+            self._sumy += value
+            self._sumy2 += value * value
+            self.result = value
+            return self.result
         else:
             last_x =  self.timestamps[int(self.age)]
             last_y = self.values[int(self.age)]
@@ -38,29 +47,36 @@ class LSMA(object):
             self._sumy -= last_y
             self._sumy2 -= last_y * last_y
 
-            self.values[int(self.age)] = float(value)
-            self.timestamps[int(self.age)] = int(ts)
+            self.values[int(self.age)] = value
+            self.timestamps[int(self.age)] = ts
 
-            self._sumx += float(ts)
-            self._sumx2 += float(ts) * float(ts)
-            self._sumxy += float(ts) * float(value)
-            self._sumy += float(value)
-            self._sumy2 += float(value) * float(value)
+            self._sumx += ts
+            self._sumx2 += ts * ts
+            self._sumxy += ts * value
+            self._sumy += value
+            self._sumy2 += value * value
 
-            denom = (self.window * self._sumx2 - self._sumx * self._sumx)
+            self.n = self.window #max(self.timestamps) - min(self.timestamps)
+
+            denom = (self.n * self._sumx2 - self._sumx * self._sumx)
             if denom == 0:
                 # singular matrix, can't solve problem
                 self.m = 0
                 self.b = 0
                 self.r = 0
-                return
+                self.result = float(value)
+                return self.result
 
-            self.m = (self.window * self._sumxy - self._sumx * self._sumy) / denom
+            self.m = (self.n * self._sumxy - self._sumx * self._sumy) / denom
             self.b = (self._sumy * self._sumx2 - self._sumx - self._sumxy) / denom
-
+            # *m = (n * sumxy - sumx * sumy) / denom;
+            # *b = (sumy * sumx2 - sumx * sumxy) / denom;
             # compute r
-            r1 = self._sumxy - (self._sumx * self._sumy) / self.window
-            r2 = np.sqrt((self._sumx2 - (self._sumx * self._sumx) / self.window) * (self._sumy2 - (self._sumy * self._sumy) / self.window))
-            self.r = r1 / r2
+            #r1 = self._sumxy - (self._sumx * self._sumy) / self.window
+            #r2 = np.sqrt((self._sumx2 - (self._sumx * self._sumx) / self.window) * (self._sumy2 - (self._sumy * self._sumy) / self.window))
+            #self.r = r1 / r2
 
         self.age = (self.age + 1) % self.window
+        self.result = self.m * float(ts) + self.b
+
+        return self.result
