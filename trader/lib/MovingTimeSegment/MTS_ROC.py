0827c6cda7c03200501996c3bf8fe41ed212cca9
Scott Glover <scottgl@gmail.com>
2019-04-12 13:59:54 -0500

    Added pchange_win_size option to MTS_ROC.

--- trader/lib/MovingTimeSegment/MTS_ROC.py
+++ trader/lib/MovingTimeSegment/MTS_ROC.py
@@ -1,14 +1,18 @@
 # Moving Time Segment version of ROC (Rate of Change)
 
 class MTS_ROC(object):
-    def __init__(self, win_secs=3600):
+    def __init__(self, win_secs=3600, pchange_win_size=0):
         self.win_secs = win_secs
         self.values = []
         self.seconds = []
         self.start_ts = 0
         self.result = 0
+        self.last_result = 0
         self.full = False
         self.start_ts = 0
+        self.pchange_win_size = pchange_win_size
+        self.pchange_values = []
+        self.pchange_age = 0
 
     def ready(self):
         return self.full
@@ -38,6 +42,17 @@ class MTS_ROC(object):
 
         # this gives percent change per win_secs, by default percent change per hour
         pchange = 100.0 * (self.values[-1] - self.values[0]) / self.values[0]
+
+        if self.pchange_win_size:
+            if len(self.pchange_values) < self.pchange_win_size:
+                self.pchange_values.append(pchange)
+            else:
+                self.last_result = self.pchange_values[int(self.pchange_age)]
+                self.pchange_values[int(self.pchange_age)] = pchange
+                self.pchange_age = (self.pchange_age + 1) % self.pchange_win_size
+        else:
+            self.last_result = self.result
+
         self.result = pchange
 
         return self.result
