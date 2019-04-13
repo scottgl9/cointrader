44a2b0794d142bbc65ee000a5c35ca78313a183e
Scott Glover <scottgl@gmail.com>
2019-04-13 18:49:56 -0500

    Updated binance_build_hourly_kline_db.py.

--- tools/binance_build_hourly_kline_db.py
+++ tools/binance_build_hourly_kline_db.py
@@ -59,10 +59,17 @@ if __name__ == '__main__':
     logger.addHandler(consoleHandler)
     logger.setLevel(logging.INFO)
 
+    db_file = "binance_hourly_klines.db"
+    if os.path.exists(db_file):
+        logger.info("{} already exists, exiting....".format(db_file))
+        sys.exit(0)
+
     client = Client(MY_API_KEY, MY_API_SECRET)
     accnt = AccountBinance(client, logger=logger)
     accnt.load_info_all_assets()
     accnt.load_detail_all_assets()
+
+    symbol_table_list = []
     for symbol in sorted(accnt.get_all_ticker_symbols('BTC')):
         if accnt.get_usdt_value_symbol(symbol) < 0.02:
             continue
@@ -71,40 +78,35 @@ if __name__ == '__main__':
         if accnt.deposit_asset_disabled(base_name):
             continue
 
-        print(symbol)
+        symbol_table_list.append(symbol)
 
-    # db_file = "binance_hourly_klines.db"
-    # if os.path.exists(db_file):
-    #     logger.info("{} already exists, exiting....".format(db_file))
-    #     sys.exit(0)
-    # db_conn = create_db_connection(db_file)
-    # cur = db_conn.cursor()
-    # cur.execute("""CREATE TABLE miniticker (E integer, c real, h real, l real, o real, q real, s text, v real)""")
-    # db_conn.commit()
+    db_conn = create_db_connection(db_file)
 
-    # start_ts = int(time.mktime(time.strptime(results.start_date, "%d/%m/%Y")))
-    # end_ts = int(time.mktime(time.strptime(results.end_date, "%d/%m/%Y")))
-    # print("Getting klines from {} to {} for {}".format(results.start_date, results.end_date, results.symbol))
-    #
-    # filename = "{}_klines.csv".format(results.symbol)
-    # f = open(filename, "wt")
-    # f.write("timestamp,open,high,low,close,base_volume,quote_volume,trade_count,taker_buy_base_volume,taker_buy_quote_volume,\n")
-    #
-    # cur_start_ts = start_ts
-    # cur_end_ts = cur_start_ts + 3600 * 500
-    #
-    #
-    # klines = client.get_historical_klines_generator(
-    #          symbol=results.symbol,
-    #          interval=Client.KLINE_INTERVAL_1HOUR,
-    #          start_str=start_ts * 1000,
-    #          end_str=end_ts * 1000,
-    # )
-    #
-    # for kline in klines:
-    #     del kline[6]
-    #     kline = kline[:-1]
-    #     f.write(",".join(map(str, kline)))
-    #     f.write(",\n")
-    #
-    # f.close()
+    start_ts = int(time.mktime(time.strptime(results.start_date, "%m/%d/%Y")))
+    end_ts = int(time.mktime(time.strptime(results.end_date, "%m/%d/%Y")))
+
+    columns = "ts integer,open real,high real,low real,close real,base_volume real,quote_volume real,trade_count integer,taker_buy_base_volume real,taker_buy_quote_volume real"
+    cnames = "ts, open, high, low, close, base_volume, quote_volume, trade_count, taker_buy_base_volume, taker_buy_quote_volume"
+
+    for symbol in symbol_table_list:
+        cur = db_conn.cursor()
+        cur.execute("""CREATE TABLE {} ({})""".format(symbol, columns))
+        db_conn.commit()
+
+        print("Getting klines from {} to {} for {}".format(results.start_date, results.end_date, symbol))
+
+        klines = client.get_historical_klines_generator(
+                 symbol=symbol,
+                 interval=Client.KLINE_INTERVAL_1HOUR,
+                 start_str=start_ts * 1000,
+                 end_str=end_ts * 1000,
+        )
+
+        sql = """INSERT INTO {} ({}) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""".format(symbol, cnames)
+
+        for k in klines:
+            del k[6]
+            k = k[:-1]
+            cur = db_conn.cursor()
+            cur.execute(sql, k)
+        db_conn.commit()
