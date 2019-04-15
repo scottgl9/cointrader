a77ead587b6977ac2879c9666741094b181cf7b4
Scott Glover <scottgl@gmail.com>
2019-04-14 21:28:22 -0500

    Updated binance_update_hourly_kline_db.py

--- tools/binance_update_hourly_kline_db.py
+++ tools/binance_update_hourly_kline_db.py
@@ -8,9 +8,16 @@ except ImportError:
     import trader
 
 import sqlite3
+import time
+import logging
 import sys
 import os
 import argparse
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from trader.config import *
+from trader.account.binance.client import Client
+from trader.account.AccountBinance import AccountBinance
 
 def get_table_list(c):
     result = []
@@ -18,6 +25,31 @@ def get_table_list(c):
     for name in res:
         result.append(name[0])
     return result
+
+def update_table(conn, client, symbol, end_ts):
+    cur = conn.cursor()
+    cur.execute("SELECT ts FROM {} ORDER BY ts DESC LIMIT 1".format(symbol))
+    result = cur.fetchone()
+    start_ts = result[0]
+    print("Getting {} through {} for {}".format(start_ts, end_ts, symbol))
+    cnames = "ts, open, high, low, close, base_volume, quote_volume, trade_count, taker_buy_base_volume, taker_buy_quote_volume"
+    klines = client.get_historical_klines_generator(
+        symbol=symbol,
+        interval=Client.KLINE_INTERVAL_1HOUR,
+        start_str=start_ts,
+        end_str=end_ts,
+    )
+
+    sql = """INSERT INTO {} ({}) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""".format(symbol, cnames)
+
+    for k in klines:
+        if k[0] == start_ts:
+            continue
+        del k[6]
+        k = k[:-1]
+        cur = conn.cursor()
+        cur.execute(sql, k)
+    conn.commit()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -47,5 +79,26 @@ if __name__ == '__main__':
     if results.list_table_names:
         for symbol in get_table_list(conn):
             print(symbol)
+        sys.exit(0)
+
+    logFormatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s]  %(message)s")
+    logger = logging.getLogger()
+
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    logger.addHandler(consoleHandler)
+    logger.setLevel(logging.INFO)
+
+    client = Client(MY_API_KEY, MY_API_SECRET)
+    accnt = AccountBinance(client, logger=logger)
+    accnt.load_info_all_assets()
+    accnt.load_detail_all_assets()
+
+    end_ts = int(time.mktime(datetime.today().timetuple()) * 1000.0)
+    print(end_ts)
+
+    if results.update:
+        for symbol in get_table_list(conn):
+            update_table(conn, client, symbol, end_ts)
 
     conn.close()
