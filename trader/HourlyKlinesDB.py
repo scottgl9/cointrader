4b2905a2a5134278885904d56a6d3045b3558885
Scott Glover <scottgl@gmail.com>
2019-04-15 00:24:57 -0500

    Added get_klines_through_ts() to HourlyKlinesDB

--- trader/HourlyKlinesDB.py
+++ trader/HourlyKlinesDB.py
@@ -1,4 +1,5 @@
 import sqlite3
+from trader.lib.Kline import Kline
 
 class HourlyKlinesDB(object):
     def __init__(self, client, filename):
@@ -7,6 +8,7 @@ class HourlyKlinesDB(object):
         self.conn = sqlite3.connect(filename)
         # column names
         self.cnames = "ts, open, high, low, close, base_volume, quote_volume, trade_count, taker_buy_base_volume, taker_buy_quote_volume"
+        self.scnames = "ts, open, high, low, close, base_volume, quote_volume"
 
     def close(self):
         if self.conn:
@@ -31,9 +33,8 @@ class HourlyKlinesDB(object):
 
     def get_raw_klines_through_ts(self, symbol, end_ts):
         result = []
-        cnames = "ts, open, high, low, close, base_volume, quote_volume"
         cur = self.conn.cursor()
-        cur.execute("SELECT {} from {}".format(cnames, symbol))
+        cur.execute("SELECT {} from {}".format(self.scnames, symbol))
         for row in cur:
             result.append(row)
             if row[0] >= end_ts:
@@ -42,14 +43,31 @@ class HourlyKlinesDB(object):
 
     def get_dict_klines_through_ts(self, symbol, end_ts):
         result = []
-        cnames = "ts, open, high, low, close, base_volume, quote_volume"
         cur = self.conn.cursor()
-        cur.execute("SELECT {} from {}".format(cnames, symbol))
+        cur.execute("SELECT {} from {}".format(self.scnames, symbol))
         for row in cur:
             msg = {}
-            for i in range(0, len(cnames)):
-                msg[cnames[i]] = row[i]
+            for i in range(0, len(self.scnames)):
+                msg[self.scnames[i]] = row[i]
             result.append(msg)
+            if row[0] >= end_ts:
+                break
+        return result
+
+    def get_klines_through_ts(self, symbol, end_ts):
+        result = []
+        cur = self.conn.cursor()
+        cur.execute("SELECT {} from {}".format(self.scnames, symbol))
+        for row in cur:
+            kline = Kline()
+            kline.ts = row[0]
+            kline.open = row[1]
+            kline.high = row[2]
+            kline.low = row[3]
+            kline.close = row[4]
+            kline.volume_base = row[5]
+            kline.volume_quote = row[6]
+            result.append(kline)
             if row[0] >= end_ts:
                 break
         return result
