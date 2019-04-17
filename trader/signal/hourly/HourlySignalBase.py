87e38eafd4c6b42fdac9ff31be31ac39001c6e93
Scott Glover <scottgl@gmail.com>
2019-04-17 15:12:49 -0500

    Added initial HourlySignalBase and Hourly_LSMA_Crossover.

---
+++ trader/signal/hourly/HourlySignalBase.py
@@ -0,0 +1,9 @@
+class HourlySignalBase(object):
+    def __init__(self, accnt=None, symbol=None, asset_info=None):
+        self.accnt = accnt
+        self.symbol = symbol
+        self.asset_info = asset_info
+        self.klines = None
+
+    def load(self, klines):
+        pass
---
+++ trader/signal/hourly/Hourly_LSMA_Crossover.py
@@ -0,0 +1,8 @@
+from .HourlySignalBase import HourlySignalBase
+
+class Hourly_LSMA_Crossover(HourlySignalBase):
+    def __init__(self, accnt=None, symbol=None, asset_info=None):
+        super(Hourly_LSMA_Crossover).__init__(accnt, symbol, asset_info)
+
+    def load(self, klines):
+        pass
