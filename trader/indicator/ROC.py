197f2b34479fe11306ad339c62bf5829c966a389
Scott Glover <scottgl@gmail.com>
2019-05-02 09:20:12 -0500

    Updated ROC indicator, and Added hourly ROC plotter

--- trader/indicator/ROC.py
+++ trader/indicator/ROC.py
@@ -1,16 +1,38 @@
 # Compute the rate of change (ROC) from current price and price N values ago
 from .IndicatorBase import IndicatorBase
+from trader.indicator.SMA import SMA
 
 
 class ROC(IndicatorBase):
-    def __init__(self, window=10):
+    def __init__(self, window=10, use_sma=False):
         IndicatorBase.__init__(self, use_close=True)
         self.window = window
-        self.result = 0.0
+        self.use_sma = use_sma
+        self.sma = None
+        if self.use_sma:
+            self.sma = SMA(window=window)
+        self.result = 0
+        self.last_value = 0
         self.age = 0
         self.values = []
 
-    def update(self, value, ts=0):
+    def update(self, value):
+        if not self.window:
+            if not self.last_value:
+                self.last_value = float(value)
+                return self.result
+            else:
+                self.result = 100.0 * (float(value - self.last_value)) / self.last_value
+                return self.result
+
+        if self.use_sma:
+            if not self.last_value:
+                self.last_value = float(value)
+                return self.result
+            else:
+                self.result = self.sma.update(100.0 * (float(value - self.last_value)) / self.last_value)
+                return self.result
+
         if len(self.values) < self.window:
             self.values.append(float(value))
             self.result = 0
