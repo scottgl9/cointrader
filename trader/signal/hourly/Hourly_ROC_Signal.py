6648a102c2a1a419aeda3f2320f0bbf9e57c2cad
Scott Glover <scottgl@gmail.com>
2019-05-17 13:49:28 -0500

    Updates to Hourly_ROC_Signal, updated cache

--- trader/signal/hourly/Hourly_ROC_Signal.py
+++ trader/signal/hourly/Hourly_ROC_Signal.py
@@ -1,11 +1,15 @@
 from .HourlySignalBase import HourlySignalBase
 from trader.indicator.ROC import ROC
+from trader.lib.Crossover import Crossover
 
 class Hourly_ROC_Signal(HourlySignalBase):
     def __init__(self, hkdb=None, accnt=None, symbol=None, asset_info=None):
         super(Hourly_ROC_Signal, self).__init__(hkdb, accnt, symbol, asset_info)
         self.name = "Hourly_ROC_Signal"
         self.roc = ROC(window=24, use_sma=True)
+        self.roc_cross = Crossover(window=2)
+        self.cross_down = False
+        self.cross_up = False
 
     def load(self, start_ts=0, end_ts=0, ts=0):
         self.klines = self.hkdb.get_dict_klines(self.symbol, start_ts=start_ts, end_ts=end_ts)
@@ -35,6 +39,7 @@ class Hourly_ROC_Signal(HourlySignalBase):
 
         close = float(kline['close'])
         self.roc.update(close)
+        self.roc_cross.update(self.roc.result, 0)
 
         self.last_hourly_ts = hourly_ts
 
@@ -45,7 +50,14 @@ class Hourly_ROC_Signal(HourlySignalBase):
         if (self.last_ts - self.last_hourly_ts) >= self.accnt.hours_to_ts(2):
             return False
 
-        if self.roc.result <= 0: # and self.roc.result <= self.roc.last_result:
+        if self.roc_cross.crossdown_detected():
+            self.cross_down = True
+            self.cross_up = False
+        elif self.roc_cross.crossup_detected():
+            self.cross_down = False
+            self.cross_up = True
+        if self.cross_down:
             return False
-
+        if not self.cross_down and self.roc.result <= 0:
+            return False
         return True
