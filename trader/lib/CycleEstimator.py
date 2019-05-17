899231113f7f470b306b23b6ea2e2e9c6017f3a6
Scott Glover <scottgl@gmail.com>
2019-05-17 16:19:19 -0500

    Updated CycleEstimator class

--- trader/lib/CycleEstimator.py
+++ trader/lib/CycleEstimator.py
@@ -2,13 +2,20 @@
 
 
 class CycleEstimator(object):
-    def __init__(self):
-        pass
+    def __init__(self, capture_rate_secs=60):
+        self.capture_rate_secs = capture_rate_secs
+        self.last_capture_ts = 0
+        self.time_price_index = {}
+        self.price_time_index = {}
 
     def update(self, value, ts):
-        pass
-
-class CyclePoint(object):
-    def __init__(self, value, ts):
-        self.value = value
-        self.ts = ts
+        if self.last_capture_ts and (ts - self.last_capture_ts) < self.capture_rate_secs * 1000:
+            return
+        self.last_capture_ts = ts
+        self.time_price_index[ts] = value
+        try:
+            timestamps = self.price_time_index[value]
+            timestamps.append(ts)
+            self.price_time_index = timestamps
+        except KeyError:
+            self.price_time_index[value] = [ts]
