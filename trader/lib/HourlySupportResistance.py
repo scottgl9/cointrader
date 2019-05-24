c48eabf7b3933cd8e2f7bde3bd6f2abfbaf93d7c
Scott Glover <scottgl@gmail.com>
2019-05-24 17:50:20 -0500

    Updated HourlySupportResistance

--- trader/lib/HourlySupportResistance.py
+++ trader/lib/HourlySupportResistance.py
@@ -3,6 +3,18 @@
 # 1) major and minor support/resistance lines in the daily (24hr) timeframe
 # 2) major and minor support/resistance lines in the weekly (168hr) timeframe
 # 3)  major and minor support/resistance lines in the monthly (730hr) timeframe
+
+# DESIGN:
+# track close prices for daily, weekly, and monthly
+# 1) if for the specified timeframe, close prices do not exceed a given high when backtesting current high,
+# then set high as new resistance line
+# 2) if for the specified timeframe, close prices do not drop below a given low when backtesting current low,
+# then set low as new support line
+# 3) With established support / resistance lines. If the price has dropped below the support line for amount of time
+# of specified timeframe, then support line converted to resistance line, and again do step #2 to get new support
+# 4) With established support / resistance lines. If the price has exceeded above the resistance line for amount of time
+# of specified timeframe, then resistance line converted to support line, and again do step #1 to get new support
+
 from trader.HourlyKlinesDB import HourlyKlinesDB
 from trader.lib.struct.CircularArray import CircularArray
 
@@ -18,10 +30,13 @@ class HourlySupportResistance(object):
         self.win_daily = 24
         self.win_weekly = 168
         self.win_monthly = 730
+        self.daily_closes = CircularArray(window=self.win_daily)
         self.daily_lows = CircularArray(window=self.win_daily)
         self.daily_highs = CircularArray(window=self.win_daily)
+        self.weekly_closes = CircularArray(window=self.win_weekly)
         self.weekly_lows = CircularArray(window=self.win_weekly)
         self.weekly_highs = CircularArray(window=self.win_weekly)
+        self.monthly_closes = CircularArray(window=self.win_monthly)
         self.monthly_lows = CircularArray(window=self.win_monthly)
         self.monthly_highs = CircularArray(window=self.win_monthly)
         self.daily_support = 0
@@ -36,35 +51,79 @@ class HourlySupportResistance(object):
             klines = self.db.get_klines(self.symbol, hourly_start_ts, hourly_end_ts)
 
         for kline in klines:
+            self.daily_closes.add(kline.close)
             self.daily_lows.add(kline.low)
             self.daily_highs.add(kline.high)
+            self.weekly_closes.add(kline.close)
             self.weekly_lows.add(kline.low)
             self.weekly_highs.add(kline.high)
+            self.monthly_closes.add(kline.close)
             self.monthly_lows.add(kline.low)
             self.monthly_highs.add(kline.high)
+
+    def find_support_resistance(self, kline, closes, support, resistance):
+        if not support:
+            if resistance:
+                if kline.low < resistance:
+                    min_daily_closes = closes.min()
+                    if min_daily_closes > kline.low:
+                        support = kline.low
+                else:
+                    resistance = 0
+            else:
+                min_daily_closes = closes.min()
+                if min_daily_closes > kline.low:
+                    support = kline.low
+
+        if not resistance:
+            if support:
+                if kline.high > support:
+                    max_daily_closes = closes.max()
+                    if max_daily_closes < kline.high:
+                        resistance = kline.high
+                else:
+                    support = 0
+            else:
+                max_daily_closes = closes.max()
+                if max_daily_closes < kline.high:
+                    resistance = kline.high
+
+        return support, resistance
 
     def update(self, hourly_ts=0, kline=None):
         if not kline:
             kline = self.db.get_kline(self.symbol, hourly_ts)
 
+        self.daily_closes.add(kline.close)
         self.daily_lows.add(kline.low)
         self.daily_highs.add(kline.high)
+        self.weekly_closes.add(kline.close)
         self.weekly_lows.add(kline.low)
         self.weekly_highs.add(kline.high)
+        self.monthly_closes.add(kline.close)
         self.monthly_lows.add(kline.low)
         self.monthly_highs.add(kline.high)
 
         if len(self.daily_lows) < self.win_daily:
             return
-        self.daily_support = self.daily_lows.min()
-        self.daily_resistance = self.daily_highs.max()
+
+        self.daily_support, self.daily_resistance = self.find_support_resistance(kline,
+                                                                                 self.daily_closes,
+                                                                                 self.daily_support,
+                                                                                 self.daily_resistance)
 
         if len(self.weekly_lows) < self.win_weekly:
             return
-        self.weekly_support = self.weekly_lows.min()
-        self.weekly_resistance = self.weekly_highs.max()
+
+        self.weekly_support, self.weekly_resistance = self.find_support_resistance(kline,
+                                                                                   self.weekly_closes,
+                                                                                   self.weekly_support,
+                                                                                   self.weekly_resistance)
 
         if len(self.monthly_lows) < self.win_monthly:
             return
-        self.monthly_support = self.monthly_lows.min()
-        self.monthly_resistance = self.monthly_highs.max()
+
+        self.monthly_support, self.monthly_resistance = self.find_support_resistance(kline,
+                                                                                     self.monthly_closes,
+                                                                                     self.monthly_support,
+                                                                                     self.monthly_resistance)
