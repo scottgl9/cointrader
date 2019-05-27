1a5a680ef5eaefc9acc892b49dd576fab896b488
Scott Glover <scottgl@gmail.com>
2019-05-27 13:58:31 -0500

    Added crontab management class with CronManage.

---
+++ trader/CronManage.py
@@ -0,0 +1,19 @@
+# Cron tab management
+from crontab import CronTab
+import getpass
+
+
+class CronManage(object):
+    def __init__(self):
+        self.username = getpass.getuser()
+        self.cron = CronTab(user=self.username)
+
+    def remove_all_jobs(self):
+        for job in self.cron:
+            self.cron.remove(job)
+        self.cron.write()
+
+    def add_hourly_job(self, cmd, args):
+        cmd = "python {} {}".format(cmd, args)
+        job = self.cron.new(command=cmd)
+        job.hour.every(1)
--- ubuntu_prereq.sh
+++ ubuntu_prereq.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get install -y python-pip python-autobahn python-flask python-twisted python-tz python-numpy python-six 
 apt-get install -y python-requests python-matplotlib python-scipy python-sklearn python-pymongo python-aniso8601
-apt-get install -y python-websocket cython python-pandas python-psycopg2 python-cryptography python-gevent
+apt-get install -y python-websocket cython python-pandas python-psycopg2 python-cryptography python-gevent python-crontab
 pip install --upgrade pip
 pip install dateparser
 pip install -r requirements.txt
