057eecae54e3dc177a45d06d5c9a3c39dc707b5b
Scott Glover <scottgl@gmail.com>
2019-06-05 15:55:30 -0500

    Updates to Crossover class. Added initial CrossAngle class.

--- trader/lib/Crossover.py
+++ trader/lib/Crossover.py
@@ -8,9 +8,10 @@
 
 
 class Crossover(object):
-    def __init__(self, pre_window=0, window=12):
+    def __init__(self, pre_window=0, window=12, detect_cross_value=False):
         self.window = window
         self.pre_window = pre_window
+        self.detect_cross_value = detect_cross_value
 
         self.values1 = []
         self.values2 = []
@@ -21,8 +22,13 @@ class Crossover(object):
         self.last_age = 0
         self.values_under = False
         self.values_over = False
+        # use for detecting the value where crossover took place
+        self.values1_under_max_value = 0
+        self.values1_over_min_value = 0
         self.crossup = False
         self.crossdown = False
+        self.crossup_value = 0
+        self.crossdown_value = 0
 
         # optimize check for min and max of self.values1
         self.values1_min_value = 0
@@ -59,27 +65,37 @@ class Crossover(object):
                 # values1 in window were under values2, now all values1 over values2
                 self.values_under = False
                 self.crossup = True
+                if self.detect_cross_value:
+                    # estimate the value where the cross-up occurred
+                    self.crossup_value = (self.values1_min_value + self.values1_under_max_value) / 2.0
             elif self.values_over and self.values1_max_value < self.values2_min_value:
                 # values1 in window were over values2, now all values1 under values2
                 self.values_over = False
                 self.crossdown = True
+                if self.detect_cross_value:
+                    # estimate the value where the cross-down occurred
+                    self.crossdown_value = (self.values1_max_value + self.values1_over_min_value) / 2.0
             elif not self.values_under and not self.values_over:
                 if self.values1_max_value < self.values2_min_value:
                     if self.pre_window:
                         # make sure values1 in pre_window were also under values2
                         if max(self.pre_values1) < min(self.pre_values2):
                             self.values_under = True
+                            self.values1_under_max_value = self.values1_max_value
                     else:
                         # all of values1 under values2
                         self.values_under = True
+                        self.values1_under_max_value = self.values1_max_value
                 elif self.values1_min_value > self.values2_max_value:
                     if self.pre_window:
                         # make sure values1 in pre_window were also over values2
                         if min(self.pre_values1) > max(self.pre_values2):
                             self.values_over = True
+                            self.values1_over_min_value = self.values1_min_value
                     else:
                         # all of values1 over values2
                         self.values_over = True
+                        self.values1_over_min_value = self.values1_min_value
 
             self.last_age = self.age
             self.age = (self.age + 1) % self.window
