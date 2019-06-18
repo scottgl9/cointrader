b2c9ed9a9d48b51817956fb5af1add2fd72b0fef
Scott Glover <scottgl@gmail.com>
2019-06-17 19:35:23 -0500

    Updates to UltimateIndicator

--- trader/lib/UltimateIndicator.py
+++ trader/lib/UltimateIndicator.py
@@ -10,7 +10,11 @@ from trader.lib.struct.CircularArray import CircularArray
 from trader.indicator.EMA import EMA
 
 class UltimateIndicator(object):
-    def __init__(self):
+    def __init__(self, hkdb=None, accnt=None, symbol=None, asset_info=None):
+        self.hkdb = hkdb
+        self.accnt = accnt
+        self.symbol = symbol
+        self.asset_info = asset_info
         # realtime members
         self.rt_ema1 = EMA(12, scale=24)
         self.rt_ema2 = EMA(50, scale=24)
@@ -28,16 +32,28 @@ class UltimateIndicator(object):
         self.hourly_ca3 = CircularArray(window=24*30, track_minmax=True) # monthly
         self.hourly_cross = Crossover3(window=12)
 
-    def update_hourly(self, ts):
-        #self.hourly_ema1.update()
-        pass
+    # hourly update
+    def update_hourly(self, hourly_ts):
+        kline = self.hkdb.get_dict_kline(self.symbol, hourly_ts)
 
-    def update_rt(self, close, ts, volume=0):
+        # hourly kline not in db yet, wait until next update() call
+        if not kline:
+            return False
+
+        self.hourly_ema1.update(kline.close)
+        self.hourly_ema2.update(kline.close)
+        self.hourly_ema3.update(kline.close)
+        self.hourly_ca1.add(self.hourly_ema1.result)
+        self.hourly_ca2.add(self.hourly_ema2.result)
+        self.hourly_ca3.add(self.hourly_ema3.result)
+        self.hourly_cross.update(self.hourly_ema1.result, self.hourly_ema2.result, self.hourly_ema3.result, kline.ts)
+
+    # realtime update
+    def update(self, close, ts, volume=0):
         self.rt_ema1.update(close, ts)
         self.rt_ema2.update(close, ts)
         self.rt_ema3.update(close, ts)
         self.rt_mts1.update(self.rt_ema1.result, ts)
         self.rt_mts2.update(self.rt_ema1.result, ts)
         self.rt_mts3.update(self.rt_ema1.result, ts)
-        self.hourly_cross.update(self.rt_ema1.result, self.rt_ema2.result, self.rt_ema3.result, ts)
-
+        self.rt_cross.update(self.rt_ema1.result, self.rt_ema2.result, self.rt_ema3.result, ts)
