29f48c07d83e0b6b26a7a1e41dc7ae873bf11196
Scott Glover <scottgl@gmail.com>
2019-06-17 19:28:16 -0500

    Updates to UltimateIndicator

--- trader/lib/UltimateIndicator.py
+++ trader/lib/UltimateIndicator.py
@@ -3,12 +3,11 @@
 # 2) hourly klines from sqlitedb
 # for realtime
 from trader.lib.MovingTimeSegment.MovingTimeSegment import MovingTimeSegment
-from trader.lib.MovingTimeSegment.MTSCrossover2 import MTSCrossover2
+from trader.lib.MovingTimeSegment.MTSCrossover3 import MTSCrossover3
 # for hourly
-from trader.lib.Crossover2 import Crossover2
+from trader.lib.Crossover3 import Crossover3
 from trader.lib.struct.CircularArray import CircularArray
 from trader.indicator.EMA import EMA
-
 
 class UltimateIndicator(object):
     def __init__(self):
@@ -19,6 +18,7 @@ class UltimateIndicator(object):
         self.rt_mts1 = MovingTimeSegment(seconds=3600, disable_fmm=False)
         self.rt_mts2 = MovingTimeSegment(seconds=3600*4, disable_fmm=False)
         self.rt_mts3 = MovingTimeSegment(seconds=3600*8, disable_fmm=False)
+        self.rt_cross = MTSCrossover3(win_secs=60)
         # hourly members
         self.hourly_ema1 = EMA(12)
         self.hourly_ema2 = EMA(50)
@@ -26,6 +26,7 @@ class UltimateIndicator(object):
         self.hourly_ca1 = CircularArray(window=24, track_minmax=True)    # daily
         self.hourly_ca2 = CircularArray(window=24*7, track_minmax=True)  # weekly
         self.hourly_ca3 = CircularArray(window=24*30, track_minmax=True) # monthly
+        self.hourly_cross = Crossover3(window=12)
 
     def update_hourly(self, ts):
         #self.hourly_ema1.update()
@@ -38,3 +39,5 @@ class UltimateIndicator(object):
         self.rt_mts1.update(self.rt_ema1.result, ts)
         self.rt_mts2.update(self.rt_ema1.result, ts)
         self.rt_mts3.update(self.rt_ema1.result, ts)
+        self.hourly_cross.update(self.rt_ema1.result, self.rt_ema2.result, self.rt_ema3.result, ts)
+
