dad4aaf0b6701645cd0256546e069b91ce336a55
Scott Glover <scottgl@gmail.com>
2019-06-28 10:44:18 -0500

    Added initial version of MTS_Retracement

---
+++ trader/lib/MovingTimeSegment/MTS_Retracement.py
@@ -0,0 +1,34 @@
+from .MovingTimeSegment import MovingTimeSegment
+from .MTSCrossover2 import MTSCrossover2
+
+
+class MTS_Retracement(object):
+    def __init__(self, win_secs=3600):
+        self.win_secs = win_secs
+        self.mts1 = MovingTimeSegment(seconds=self.win_secs, disable_fmm=False)
+        self.mts2 = MovingTimeSegment(seconds=self.win_secs, disable_fmm=False)
+        self.cross_up = MTSCrossover2(win_secs=60)
+        self.cross_down = MTSCrossover2(win_secs=60)
+        self.result = 0
+        self.trend_up = False
+        self.trend_down = False
+
+    def update(self, value, ts):
+        self.mts1.update(value, ts)
+        if not self.mts1.ready():
+            return self.result
+
+        self.mts2.update(self.mts1.first_value(), ts)
+        if not self.mts2.ready():
+            return self.result
+
+        self.cross_up.update(self.mts2.last_value(), self.mts1.max(), ts)
+        self.cross_down.update(self.mts2.last_value(), self.mts1.min(), ts)
+
+        if self.cross_up.crossup_detected():
+            self.trend_up = True
+            self.trend_down = False
+
+        if self.cross_down.crossdown_detected():
+            self.trend_up = False
+            self.trend_down = True
