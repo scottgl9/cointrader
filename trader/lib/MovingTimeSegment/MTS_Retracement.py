06e63a565f3de7ed597806ec2da078bd46a97273
Scott Glover <scottgl@gmail.com>
2019-07-01 12:46:56 -0500

    Updates to MTS_Retracement

--- trader/lib/MovingTimeSegment/MTS_Retracement.py
+++ trader/lib/MovingTimeSegment/MTS_Retracement.py
@@ -5,18 +5,24 @@ from .MTSCrossover2 import MTSCrossover2
 class MTS_Retracement(object):
     def __init__(self, win_secs=3600):
         self.win_secs = win_secs
-        self.mts1 = MovingTimeSegment(seconds=self.win_secs, disable_fmm=False)
-        self.mts2 = MovingTimeSegment(seconds=self.win_secs, disable_fmm=False)
-        self.mts3 = MovingTimeSegment(seconds=self.win_secs, disable_fmm=False)
+        self.mts1 = MovingTimeSegment(seconds=self.win_secs, disable_fmm=False, track_ts=True)
+        self.mts2 = MovingTimeSegment(seconds=self.win_secs, disable_fmm=False, track_ts=True)
+        self.mts3 = MovingTimeSegment(seconds=self.win_secs, disable_fmm=False, track_ts=True)
         self.cross_up = MTSCrossover2(win_secs=60)
         self.cross_down = MTSCrossover2(win_secs=60)
         self.long_cross_down =  MTSCrossover2(win_secs=60)
         self.result = 0
-        self.prev_trend_up = False
-        self.prev_trend_down = False
-        self.trend_up = False
-        self.trend_down = False
-        self.long_trend_down = False
+        self._prev_crossup = False
+        self._prev_crossdown = False
+        self._crossup = False
+        self._crossdown = False
+        self._long_crossdown = False
+        self._crossup_value = 0
+        self._crossdown_value = 0
+        self._long_crossdown_value = 0
+        self._crossup_ts = 0
+        self._crossdown_ts = 0
+        self._long_crossdown_ts = 0
 
     def update(self, value, ts):
         self.mts1.update(value, ts)
@@ -33,39 +39,45 @@ class MTS_Retracement(object):
         #self.cross.update(self.mts1.last_value(), self.mts2.first_value(), ts)
         #self.cross_down.update(self.mts1.last_value(), self.mts2.first_value(), ts)
 
-        if self.cross_up.crossup_detected():
-            self.prev_trend_up = self.trend_up
-            self.prev_trend_down = self.trend_down
-            self.trend_up = True
-            self.trend_down = False
+        if  self.cross_up.crossup_detected():
+            self._prev_crossup = self._crossup
+            self._prev_crossdown = self._crossdown
+            self._crossup = True
+            self._crossdown = False
+            self._crossup_ts = ts
+            self._crossup_value = self.mts1.last_value()
 
         if self.cross_down.crossdown_detected():
-            self.prev_trend_up = self.trend_up
-            self.prev_trend_down = self.trend_down
-            self.trend_up = False
-            self.trend_down = True
+            self._prev_crossup = self._crossup
+            self._prev_crossdown = self._crossdown
+            self._crossup = False
+            self._crossdown = True
+            self._crossdown_ts = ts
+            self._crossdown_value = self.mts1.last_value()
 
         self.mts3.update(self.mts2.first_value(), ts)
         if self.mts3.ready():
             self.long_cross_down.update(self.mts1.last_value(), self.mts3.min(), ts)
             if self.long_cross_down.crossdown_detected():
-                self.trend_up = False
-                self.long_trend_down = True
+                self._crossup = False
+                self._long_crossdown = True
+                self._long_crossdown_ts = ts
+                self._long_crossdown_value = self.mts1.last_value()
 
     def crossup_detected(self, clear=True):
-        result = self.trend_up
+        result = self._crossup
         if clear:
-            self.trend_up = False
+            self._crossup = False
         return result
 
     def crossdown_detected(self, clear=True):
-        result = self.trend_down
+        result = self._crossdown
         if clear:
-            self.trend_down = False
+            self._crossdown = False
         return result
 
     def long_crossdown_detected(self, clear=True):
-        result = self.long_trend_down
+        result = self._long_crossdown
         if clear:
-            self.long_trend_down = False
+            self._long_crossdown = False
         return result
