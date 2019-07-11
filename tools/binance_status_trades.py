219cfd442ddb20e3aef3d6fdca3f00d541ffb48b
Scott Glover <scottgl@gmail.com>
2019-07-11 16:26:01 -0500

    Updates to binance_status_trades.py

--- tools/binance_status_trades.py
+++ tools/binance_status_trades.py
@@ -14,15 +14,28 @@ from trader.lib.TraderDB import TraderDB
 from trader.config import *
 
 if __name__ == '__main__':
+    client = client.Client(MY_API_KEY, MY_API_SECRET)
+
     if not os.path.exists('trade.db'):
         print("trade.db doesn't exist, exiting")
         sys.exit(-1)
-    client = client.Client(MY_API_KEY, MY_API_SECRET)
+
     accnt = AccountBinance(client)
     traderdb = TraderDB(filename='trade.db')
     traderdb.connect()
     trades = traderdb.get_all_trades()
     traderdb.close()
 
+    tickers = client.get_all_tickers()
+
     for trade in trades:
-        print(trade)
+        symbol = trade['symbol']
+        buy_price = float(trade['price'])
+        buy_size = float(trade['size'])
+        try:
+            price = float(tickers[symbol])
+        except KeyError:
+            continue
+
+        pchange = round(100.0 * (price - buy_price) / buy_price, 2)
+        print("{}: {}%".format(symbol, pchange))
