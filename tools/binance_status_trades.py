83da01f85bbdef01457ce663769e183dcfb70eef
Scott Glover <scottgl@gmail.com>
2019-07-11 16:17:56 -0500

    Added initial version of tool binance_status_trades.py

---
+++ tools/binance_status_trades.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# get current percent loss/profit of open trades from trade.db
+
+import sys
+import os
+try:
+    import trader
+except ImportError:
+    sys.path.append('.')
+
+from trader.account.AccountBinance import AccountBinance
+from trader.account.binance import client
+from trader.lib.TraderDB import TraderDB
+from trader.config import *
+
+if __name__ == '__main__':
+    if not os.path.exists('trade.db'):
+        print("trade.db doesn't exist, exiting")
+        sys.exit(-1)
+    client = client.Client(MY_API_KEY, MY_API_SECRET)
+    accnt = AccountBinance(client)
+    traderdb = TraderDB(filename='trade.db')
+    traderdb.connect()
+    trades = traderdb.get_all_trades()
+    traderdb.close()
+
+    for trade in trades:
+        print(trade)
