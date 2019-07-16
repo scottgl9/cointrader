49b08eeffc3bc08631694c8ad74cc9a80fef8638
Scott Glover <scottgl@gmail.com>
2019-07-16 17:12:01 -0500

    Added misc script test_all_signals.sh

---
+++ misc/test_all_signals.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "$0: <db_file>"
+	exit 0
+fi
+
+# get list of all signals
+CURPWD=`pwd`
+DB_FILE=$CURPWD/$1
+echo "Testing all signals on $DB_FILE..."
+SIGNALS_PATH=$CURPWD/trader/signal
+
+for filename in $(find $SIGNALS_PATH -maxdepth 1 -name "*.py")
+do
+	signal=$(basename $filename | sed 's/.py//')
+	if [[ $signal = '__init__' ]]; then
+		continue
+	elif [[ $signal = 'NULL_Signal' ]]; then
+	       	continue
+       	fi
+	echo "Testing signal $signal..."
+	python $CURPWD/tools/binance_simulate.py -f $DB_FILE -g $signal 
+done
