52de7e010b8c61f4158c67fcd53c8c376fd89973
Scott Glover <scottgl@gmail.com>
2019-07-31 08:44:58 -0500

    Added DELTA indicator

---
+++ trader/indicator/DELTA.py
@@ -0,0 +1,33 @@
+# Compute the delta price from current price and price N values ago
+from trader.lib.struct.IndicatorBase import IndicatorBase
+
+
+class DELTA(IndicatorBase):
+    def __init__(self, window=10, smoother=None):
+        IndicatorBase.__init__(self, use_close=True)
+        self.window = window
+        self.result = 0
+        self.last_result = 0
+        self.last_value = 0
+        self.age = 0
+        self.values = []
+        self.smoother = smoother
+
+    def update(self, close):
+        if len(self.values) < self.window:
+            self.values.append(float(close))
+            self.result = 0
+        else:
+            old_value = self.values[int(self.age)]
+            if old_value:
+                result = float(close) - old_value
+                if self.smoother:
+                    self.result = self.smoother.update(result)
+                else:
+                    self.result = result
+            else:
+                self.result = 0
+            self.values[int(self.age)] = float(close)
+            self.age = (self.age + 1) % self.window
+
+        return self.result
