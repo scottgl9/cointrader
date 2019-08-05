780e4b7646a20afe64a576cfe0969edc9214cbbc
Scott Glover <scottgl@gmail.com>
2019-08-05 13:09:46 -0500

    Added initial folder for reverse_currency_strategy

---
+++ trader/strategy/reverse_currency_strategy/__init__.py
@@ -0,0 +1,4 @@
+# trading strategies which operate in the reverse way that normal trading works
+# 1) Treat all currency balances as executed buy trades which are available to sell:
+#    For example, if we have 0.2 BTC, can sell as USDT for BTC/USDT, then re-buy later as BTC
+# 2) Ability to sell, then re-buy currencies. Actions triggered by signals
