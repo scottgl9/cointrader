e702a4268c8a766fefd08b440f6df8ecae57298b
Scott Glover <scottgl@gmail.com>
2019-08-09 18:07:20 -0500

    Added fix_cuda_10_1.sh misc script

---
+++ misc/fix_cuda_10_1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo ln -s /usr/lib/x86_64-linux-gnu/libcublas.so /usr/lib/x86_64-linux-gnu/libcublas.so.10.0
+sudo ln -s /usr/lib/x86_64-linux-gnu/libcufft.so.10 /usr/lib/x86_64-linux-gnu/libcufft.so.10.0
+sudo ln -s /usr/lib/x86_64-linux-gnu/libcurand.so.10 /usr/lib/x86_64-linux-gnu/libcurand.so.10.0
+sudo ln -s /usr/lib/x86_64-linux-gnu/libcusolver.so.10 /usr/lib/x86_64-linux-gnu/libcusolver.so.10.0
+sudo ln -s /usr/lib/x86_64-linux-gnu/libcusparse.so.10 /usr/lib/x86_64-linux-gnu/libcusparse.so.10.0
+sudo ln -s /usr/lib/x86_64-linux-gnu/libcudart.so.10.1 /usr/lib/x86_64-linux-gnu/libcudart.so.10.0
