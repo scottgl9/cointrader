12a4c564e276de0a62eb2cb8c33112facaa68e45
Scott Glover <scottgl@gmail.com>
2019-08-13 13:57:41 -0500

    Added ml_test_hourly_lstm_macd_univariate_multistep_encoder_decoder.py

---
+++ ml_tests/ml_test_hourly_lstm_macd_univariate_multistep_encoder_decoder.py
@@ -0,0 +1,241 @@
+#!/usr/bin/python
+# test HourlyLSTM class
+
+import sys
+try:
+    import trader
+except ImportError:
+    sys.path.append('.')
+    import trader
+
+import sqlite3
+import sys
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import time
+import numpy as np
+import pandas as pd
+from trader.account.binance.client import Client
+from trader.config import *
+import matplotlib.pyplot as plt
+import argparse
+from trader.lib.MachineLearning.HourlyLSTM import HourlyLSTM
+from trader.HourlyKlinesDB import HourlyKlinesDB
+from trader.account.AccountBinance import AccountBinance
+from trader.lib.DataFrameMLHelper import DataFrameMLHelper
+from trader.lib.Crossover2 import Crossover2
+from trader.lib.Indicator import Indicator
+from trader.indicator.LSMA import LSMA
+from trader.indicator.ADL import ADL
+from trader.indicator.ATR import ATR
+from trader.indicator.EFI import EFI
+from trader.indicator.EMA import EMA
+from trader.indicator.KST import KST
+from trader.indicator.MACD import MACD
+from trader.indicator.McGinleyDynamic import McGinleyDynamic
+from trader.indicator.OBV import OBV
+from trader.indicator.PPO import PPO
+from trader.indicator.ROC import ROC
+from trader.indicator.RSI import RSI
+from trader.indicator.TSI import TSI
+
+from numpy import hstack
+from numpy import insert
+from keras.preprocessing.sequence import TimeseriesGenerator
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.layers import TimeDistributed, RepeatVector
+from keras.utils import to_categorical
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
+
+def process_raw_klines(df, indicators=None):
+    # process EMA close values
+    indicator_close = Indicator(EMA, 12) #McGinleyDynamic, 14, k=1.0)
+    if indicators:
+        indicator_close_indicator = indicators['CLOSE']
+        indicator_close.set_indicator(indicator_close_indicator)
+    indicator_close.load_dataframe(df)
+    df['CLOSE'] = np.array(indicator_close.results())
+    indicator_close_indicator = indicator_close.indicator
+
+    if not indicators:
+        indicators = {}
+    indicators['CLOSE'] = indicator_close_indicator
+
+    return df, indicators
+
+
+def create_labels(df_train):
+    df_result = pd.DataFrame()
+    df_result['MACD'] = df_train['MACD']
+    return df_result
+
+
+def create_features(df, indicators=None):
+    df_result = pd.DataFrame()
+
+    # process MACD values
+    macd = Indicator(MACD) #, scale=12)
+    macd.close_key = "CLOSE"
+    try:
+        macd_indicator = indicators['MACD']
+        macd.set_indicator(macd_indicator)
+    except KeyError:
+        pass
+    macd.load_dataframe(df)
+    macd_result = np.array(macd.results(0))
+    macd_result[macd_result == 0] = np.nan
+    macd_sig_result = np.array(macd.results(1))
+    macd_sig_result[macd_sig_result == 0] = np.nan
+    df_result['MACD'] = macd_result
+    indicator_macd = macd.indicator
+
+    if not indicators:
+        indicators = {}
+    indicators['MACD'] = indicator_macd
+
+    df_result = df_result.dropna()
+
+    return df_result, indicators
+
+
+# split a univariate sequence into samples
+def split_sequence(sequence, n_steps_in, n_steps_out):
+    X, y = list(), list()
+    for i in range(len(sequence)):
+        # find the end of this pattern
+        end_ix = i + n_steps_in
+        out_end_ix = end_ix + n_steps_out
+        # check if we are beyond the sequence
+        if out_end_ix > len(sequence):
+            break
+        # gather input and output parts of the pattern
+        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix:out_end_ix]
+        X.append(seq_x)
+        y.append(seq_y)
+    return np.array(X), np.array(y)
+
+
+def simulate(hkdb, symbol, train_start_ts, train_end_ts, test_start_ts, test_end_ts):
+    mlhelper = DataFrameMLHelper()
+    xscaler = MinMaxScaler(feature_range=(0, 1))
+    yscaler = MinMaxScaler(feature_range=(0, 1))
+
+    df = hkdb.get_pandas_klines(symbol, train_start_ts, train_end_ts)
+    df, indicators = process_raw_klines(df)
+    df_train, indicators = create_features(df, indicators)
+
+    n_features = 1
+    n_steps_in, n_steps_out = 3, 2
+
+    X, Y = split_sequence(df_train['MACD'], n_steps_in, n_steps_out)
+
+    scaleX = xscaler.fit_transform(X)
+    scaleY = yscaler.fit_transform(Y)
+
+    trainX = scaleX.reshape((scaleX.shape[0], scaleX.shape[1], n_features))
+    trainy = scaleY.reshape((scaleY.shape[0], scaleY.shape[1], n_features))
+
+    model = Sequential()
+    model.add(LSTM(100, activation='relu', input_shape=(n_steps_in, n_features)))
+    model.add(RepeatVector(n_steps_out))
+    model.add(LSTM(100, activation='relu', return_sequences=True))
+    model.add(TimeDistributed(Dense(1)))
+    model.compile(optimizer='adam', loss='mse')
+    # fit model
+    #model.fit_generator(generator, steps_per_epoch=1, epochs=500, verbose=1)
+    model.fit(trainX, trainy, epochs=10, verbose=1)
+
+    y_act = []
+    y_act2 = []
+    y_pred = []
+    y_pred2 = []
+    prices = []
+    ts = test_start_ts
+    while ts <= test_end_ts:
+        start_ts = ts
+        end_ts = ts + 1000 * 3600 * (n_steps_in - 1)
+        df2 = hkdb.get_pandas_klines(symbol, start_ts, end_ts)
+        df2, indicators = process_raw_klines(df2, indicators)
+        test_df, indicators = create_features(df2, indicators)
+        test_labels_df = create_labels(test_df)
+        if test_labels_df['MACD'].size:
+            y_act.append(test_labels_df['MACD'].values[-1])
+        #if test_labels_df['MHIST'].size:
+        #    y_act2.append(test_labels_df['MHIST'].values[-1])
+        if df2['CLOSE'].size:
+            prices.append(df2['CLOSE'].values[-1])
+       # test_df = test_df.drop(columns="CLOSE")
+        try:
+            test_dataset = np.array([xscaler.transform(test_df.values)])
+            prediction = yscaler.inverse_transform(model.predict(test_dataset))
+            print(prediction)
+            y_pred.append(prediction[0][0])
+            #y_pred2.append(prediction[0][1])
+        except ValueError:
+            pass
+        ts += 1000 * 3600
+
+    plt.subplot(211)
+    #for i in crossups:
+    #    plt.axvline(x=i, color='green')
+    #for i in crossdowns:
+    #    plt.axvline(x=i, color='red')
+    fig1, = plt.plot(prices, label=symbol)
+    plt.legend(handles=[fig1])
+    plt.subplot(212)
+    fig21, = plt.plot(y_act, label='act')
+    fig22, = plt.plot(y_pred, label='pred')
+    plt.legend(handles=[fig21, fig22])
+    plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-k', action='store', dest='hourly_filename',
+                        default='binance_hourly_klines.db',
+                        help='filename of hourly kline sqlite db')
+
+    parser.add_argument('-p', action='store', dest='split_percent',
+                        default='70',
+                        help='Percent of klines to use for training (remaining used for test')
+
+    parser.add_argument('-s', action='store', dest='symbol',
+                        default='',
+                        help='trade symbol')
+
+    results = parser.parse_args()
+    if not results.symbol:
+        parser.print_help()
+        sys.exit(0)
+
+    accnt = AccountBinance(None, simulation=True)
+
+    if not os.path.exists(results.hourly_filename):
+        print("file {} doesn't exist, exiting...".format(results.filename))
+        sys.exit(-1)
+
+
+    hkdb = HourlyKlinesDB(accnt, results.hourly_filename, None)
+    print("Loading {}".format(results.hourly_filename))
+
+    total_row_count = hkdb.get_table_row_count(results.symbol)
+    train_end_index = int(total_row_count * float(results.split_percent) / 100.0)
+
+    train_start_ts = hkdb.get_table_start_ts(results.symbol)
+    train_end_ts = hkdb.get_table_ts_by_offset(results.symbol, train_end_index)
+    test_start_ts = hkdb.get_table_ts_by_offset(results.symbol, train_end_index + 1)
+    test_end_ts = hkdb.get_table_end_ts(results.symbol)
+
+    if results.symbol:
+        simulate(hkdb, results.symbol, train_start_ts, train_end_ts, test_start_ts, test_end_ts)
+    else:
+        parser.print_help()
+    hkdb.close()
