f7c0e93b28072b8554d956464bc3cf5bd3ed3a21
Scott Glover <scottgl@gmail.com>
2019-08-16 16:01:03 -0500

    Enabled GPU detection in ml_test_hourly_rl_dqn_test.py

--- ml_tests/ml_test_hourly_rl_dqn_test.py
+++ ml_tests/ml_test_hourly_rl_dqn_test.py
@@ -20,6 +20,7 @@ from trader.HourlyKlinesDB import HourlyKlinesDB
 from trader.account.AccountBinance import AccountBinance
 import keras
 import tensorflow as tf
+from tensorflow.python.client import device_lib
 import numpy as np
 from trader.lib.MachineLearning.DQNAgent import DQNAgent
 from sklearn.preprocessing import MinMaxScaler
@@ -58,7 +59,7 @@ def getState(data, t, n):
 
 
 def simulate(hkdb, symbol, train_start_ts, train_end_ts, test_start_ts, test_end_ts, train=True):
-    df_train = hkdb.get_pandas_klines(symbol, train_start_ts, train_end_ts)
+    df_train = hkdb.get_pandas_daily_klines(symbol, train_start_ts, train_end_ts)
     window_size = 10
     episode_count = 1000
     scaler = MinMaxScaler(feature_range=(0, 1))
@@ -75,7 +76,7 @@ def simulate(hkdb, symbol, train_start_ts, train_end_ts, test_start_ts, test_end
         total_profit = 0
         agent.inventory = []
 
-        for t in xrange(l):
+        for t in xrange(l - window_size):
             action = agent.act(state)
 
             # sit
@@ -85,25 +86,26 @@ def simulate(hkdb, symbol, train_start_ts, train_end_ts, test_start_ts, test_end
             if action == 1: # buy
                 if len(agent.inventory) < agent.max_inventory:
                     agent.inventory.append(data[t])
-                    buy_price = scaler.inverse_transform([[data[t]]])[0][0]
-                    print("Buy: {}".format(buy_price))
+                    #buy_price = scaler.inverse_transform([[data[t]]])[0][0]
+                    #print("Buy: {}".format(buy_price))
 
             elif action == 2 and len(agent.inventory) > 0: # sell
                 bought_price = agent.inventory.pop(0)
                 reward = max(data[t] - bought_price, 0)
                 total_profit += data[t] - bought_price
-                sell_price = scaler.inverse_transform([[data[t]]])[0][0]
-                buy_price = scaler.inverse_transform([[bought_price]])[0][0]
-                print("Sell: {} | Profit: {}".format(sell_price, sell_price - buy_price))
+                #sell_price = scaler.inverse_transform([[data[t]]])[0][0]
+                #buy_price = scaler.inverse_transform([[bought_price]])[0][0]
+                #print("Sell: {} | Profit: {}".format(sell_price, sell_price - buy_price))
                 #print "Sell: {}" + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price)
 
-            done = True if t == l - 1 else False
+            done = True if t == l - window_size - 1 else False
             agent.memory.append((state, action, reward, next_state, done))
             state = next_state
 
             if done:
                 print "--------------------------------"
-                print "Total Profit: " + formatPrice(total_profit)
+                true_total_profit = scaler.inverse_transform([[total_profit]])[0][0]
+                print "Total Profit: {}".format(true_total_profit)
                 print "--------------------------------"
 
             if len(agent.memory) > batch_size:
@@ -160,9 +162,17 @@ if __name__ == '__main__':
     test_start_ts = hkdb.get_table_ts_by_offset(results.symbol, train_end_index + 1)
     test_end_ts = hkdb.get_table_end_ts(results.symbol)
 
-    config = tf.ConfigProto(device_count={'GPU': 1, 'CPU': 8})
-    sess = tf.Session(config=config)
-    keras.backend.set_session(sess)
+    enable_gpu = False
+    device_types = []
+    for device in device_lib.list_local_devices():
+        device_types.append(device.device_type)
+    if 'GPU' in device_types or 'XLA_GPU' in device_types:
+        enable_gpu = True
+    if enable_gpu:
+        print("GPU detected, enabling tensorflow GPU...")
+        config = tf.ConfigProto(device_count={'GPU': 1, 'CPU': 8})
+        sess = tf.Session(config=config)
+        keras.backend.set_session(sess)
 
     train = True
     if results.verify:
