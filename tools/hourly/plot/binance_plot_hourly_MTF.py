719152463d5b23afa7ca9d5c77c51383e564a472
Scott Glover <scottgl@gmail.com>
2019-08-30 17:06:33 -0500

    Added TIB plotter and MTF plotter

---
+++ tools/hourly/plot/binance_plot_hourly_MTF.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python
+# Markov Transition Field (MTF) Image
+
+import sys
+try:
+    import trader
+except ImportError:
+    sys.path.append('.')
+    import trader
+
+import sqlite3
+import sys
+import os
+import numpy as np
+from datetime import datetime
+import time
+from trader.account.binance.client import Client
+from trader.config import *
+import matplotlib.pyplot as plt
+from pyts.image import MarkovTransitionField
+from pyts.datasets import load_gunpoint
+import argparse
+from trader.HourlyKlinesDB import HourlyKlinesDB
+
+def simulate(hkdb, symbol, start_ts, end_ts):
+    msgs = hkdb.get_dict_klines(symbol, start_ts, end_ts)
+
+    close_prices = []
+    open_prices = []
+    low_prices = []
+    high_prices = []
+    volumes = []
+
+    i=0
+    for msg in msgs: #get_rows_as_msgs(c):
+        ts = int(msg['ts'])
+        close = float(msg['close'])
+        low = float(msg['low'])
+        high = float(msg['high'])
+        open = float(msg['open'])
+        volume = float(msg['quote_volume'])
+        volumes.append(volume)
+
+        close_prices.append(close)
+        open_prices.append(open)
+        low_prices.append(low)
+        high_prices.append(high)
+        i += 1
+
+    # Parameters
+    n_samples, n_features = 100, 144
+
+    # Toy dataset
+    rng = np.random.RandomState(41)
+    X = rng.randn(n_samples, n_features)
+
+    # MTF transformation
+    mtf = MarkovTransitionField(image_size=128)
+    X_mtf = mtf.fit_transform(X)
+    # Show the image for the first time series
+    plt.subplot(211)
+    plt.plot(close_prices)
+    plt.subplot(212)
+    #plt.figure(figsize=(6, 6))
+    plt.imshow(X_mtf[0], cmap='rainbow', origin='lower')
+    #plt.title('Markov Transition Field', fontsize=18)
+    plt.colorbar(fraction=0.0457, pad=0.04)
+    plt.show()
+
+
+# get first timestamp from kline sqlite db
+def get_first_timestamp(filename, symbol):
+    conn = sqlite3.connect(filename)
+    c = conn.cursor()
+    c.execute("SELECT E FROM miniticker WHERE s='{}' ORDER BY E ASC LIMIT 1".format(symbol))
+    result = c.fetchone()
+    return int(result[0])
+
+def get_last_timestamp(filename, symbol):
+    conn = sqlite3.connect(filename)
+    c = conn.cursor()
+    #c.execute("SELECT ts FROM {} ORDER BY ts DESC LIMIT 1".format(symbol))
+    c.execute("SELECT E FROM miniticker WHERE s='{}' ORDER BY E DESC LIMIT 1".format(symbol))
+    result = c.fetchone()
+    return int(result[0])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # used to get first timestamp for symbol from precaptured live market data
+    parser.add_argument('-f', action='store', dest='filename',
+                        default='',
+                        help='filename of kline sqlite db')
+
+    parser.add_argument('--hours', action='store', dest='hours',
+                        default=48,
+                        help='Hours before first ts in db specified by -f')
+
+    parser.add_argument('-k', action='store', dest='hourly_filename',
+                        default='binance_hourly_klines.db',
+                        help='filename of hourly kline sqlite db')
+
+    parser.add_argument('-s', action='store', dest='symbol',
+                        default='',
+                        help='trade symbol')
+
+    parser.add_argument('-l', action='store_true', dest='list_table_names',
+                        default=False,
+                        help='List table names in db')
+
+    parser.add_argument('--start-date', action='store', dest='start_date',
+                        default='',
+                        help='specify start date in month/day/year format')
+
+    parser.add_argument('--end-date', action='store', dest='end_date',
+                        default='',
+                        help='specify end date in month/day/year format')
+
+    results = parser.parse_args()
+
+
+    hourly_filename = results.hourly_filename
+    symbol = results.symbol
+    start_ts = 0
+    end_ts = 0
+
+    if results.filename:
+        if not os.path.exists(results.filename):
+            print("file {} doesn't exist, exiting...".format(results.filename))
+            sys.exit(-1)
+        else:
+            end_ts = get_last_timestamp(results.filename, symbol)
+            start_ts = get_first_timestamp(results.filename, symbol)
+            start_ts = start_ts - 1000 * 3600 * int(results.hours)
+            print(start_ts, end_ts)
+    elif results.start_date and results.end_date:
+        start_dt = datetime.strptime(results.start_date, '%m/%d/%Y')
+        end_dt = datetime.strptime(results.end_date, '%m/%d/%Y')
+        start_ts = int(time.mktime(start_dt.timetuple()) * 1000.0)
+        end_ts = int(time.mktime(end_dt.timetuple()) * 1000.0)
+
+    if not os.path.exists(results.hourly_filename):
+        print("file {} doesn't exist, exiting...".format(results.filename))
+        sys.exit(-1)
+
+    hkdb = HourlyKlinesDB(None, hourly_filename, None)
+    print("Loading {}".format(hourly_filename))
+
+    if results.list_table_names:
+        for symbol in hkdb.get_table_list():
+            print(symbol)
+
+    if symbol:
+        simulate(hkdb, symbol, start_ts, end_ts)
+    else:
+        parser.print_help()
+    hkdb.close()
---
+++ tools/plot/binance_plot_simulate_TIB.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+
+import sys
+
+try:
+    import trader
+except ImportError:
+    sys.path.append('.')
+    import trader
+
+import sqlite3
+import sys
+import os
+import matplotlib.pyplot as plt
+import argparse
+from trader.lib.TIB import TIB
+
+
+def get_rows_as_msgs(c):
+    msgs = []
+    for row in c:
+        msg = {'E': row[0], 'c': row[1], 'h': row[2], 'l': row[3],
+               'o': row[4], 'q': row[5], 's': row[6], 'v': row[7]}
+        msgs.append(msg)
+    return msgs
+
+
+def simulate(conn, client, base=None, currency=None, ticker_id=None):
+    c = conn.cursor()
+    #c.execute("SELECT * FROM miniticker WHERE s='{}' ORDER BY E ASC".format(ticker_id))
+    c.execute("SELECT E,c,h,l,o,q,s,v FROM miniticker WHERE s='{}'".format(ticker_id)) # ORDER BY E ASC")")
+
+    tib = TIB(window=50)
+    tib_values = []
+
+    close_prices = []
+    open_prices = []
+    low_prices = []
+    high_prices = []
+    volumes = []
+
+    i=0
+    for msg in get_rows_as_msgs(c):
+        close = float(msg['c'])
+        low = float(msg['l'])
+        high = float(msg['h'])
+        open = float(msg['o'])
+        volume = float(msg['v'])
+        ts=int(msg['E'])
+        volumes.append(volume)
+
+        tib.update(close)
+        tib_values.append(tib.result)
+
+        close_prices.append(close)
+        open_prices.append(open)
+        low_prices.append(low)
+        high_prices.append(high)
+        #lstsqs_x_values.append(i)
+        i += 1
+
+    plt.subplot(211)
+    symprice, = plt.plot(close_prices, label=ticker_id)
+    plt.legend(handles=[symprice])
+    plt.subplot(212)
+    fig21, = plt.plot(tib_values, label='TIB')
+    plt.legend(handles=[fig21])
+    #plt.plot(signal_values)
+    #plt.plot(tsi_values)
+    plt.show()
+
+if __name__ == '__main__':
+    client = None
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', action='store', dest='filename',
+                        default='cryptocurrency_database.miniticker_collection_04092018.db',
+                        help='filename of kline sqlite db')
+
+    parser.add_argument('-b', action='store', dest='base',
+                        default='BTC',
+                        help='base part of symbol')
+
+    parser.add_argument('-c', action='store', dest='currency',
+                        default='USDT',
+                        help='currency part of symbol')
+
+    symbol_default = 'BTCUSDT'
+    parser.add_argument('-s', action='store', dest='symbol',
+                        default=symbol_default,
+                        help='trade symbol')
+
+    results = parser.parse_args()
+
+    if not os.path.exists(results.filename):
+        print("file {} doesn't exist, exiting...".format(results.filename))
+        sys.exit(-1)
+
+    filename = results.filename
+    base = results.base
+    currency = results.currency
+
+    print("Loading {}".format(filename))
+    conn = sqlite3.connect(filename)
+
+    ticker_id = "{}{}".format(base, currency)
+    if results.symbol != symbol_default:
+        ticker_id = results.symbol
+    simulate(conn, client, ticker_id=ticker_id)
+    conn.close()
