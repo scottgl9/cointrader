5e94c2e069726f36281d4e255998191ea3ab125f
Scott Glover <scottgl@gmail.com>
2019-09-10 19:44:51 -0500

    Updates to binance_build_hourly_kline_db.py

--- tools/binance_build_hourly_kline_db.py
+++ tools/binance_build_hourly_kline_db.py
@@ -136,14 +136,21 @@ if __name__ == '__main__':
             cur_ts = int(k[0])
             # skip if is not an hourly ts
             if not accnt.is_hourly_ts(cur_ts):
+                # check if the timestamp is less than 1 hr from last_ts or from cur_ts
+                #hourly_ts = accnt.get_hourly_ts(cur_ts)
+                #if int(hourly_ts - last_ts) < 3600000 or int(cur_ts - hourly_ts) < 3600000:
                 print("{}: skipping {}".format(symbol, cur_ts))
                 continue
+                # correct ts
+                #k[0] = int(hourly_ts)
+                #cur_ts = int(k[0])
             cur = db_conn.cursor()
             # check for gaps in hourly klines, for gaps fill with previous kline
             if last_kline and int(cur_ts - last_ts) != 3600000:
                 print("{}: gap from {} to {}, filling...".format(symbol, last_ts, cur_ts))
                 ts = last_ts
                 while ts < cur_ts:
+                    k[0] = int(ts)
                     cur.execute(sql, last_kline)
                     ts += 3600000
             cur.execute(sql, k)
