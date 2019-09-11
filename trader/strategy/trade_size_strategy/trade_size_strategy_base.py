bde4e07bcd7d77f2cdf7ba63d76c89a5be38daf8
Scott Glover <scottgl@gmail.com>
2019-09-11 14:23:36 -0500

    Updated asset_detail.json, asset_info.json, and cache

--- trader/strategy/trade_size_strategy/trade_size_strategy_base.py
+++ trader/strategy/trade_size_strategy/trade_size_strategy_base.py
@@ -2,6 +2,8 @@ class trade_size_strategy_base(object):
     def __init__(self, accnt, asset_info):
         self.accnt = accnt
         self.asset_info = asset_info
+        if not self.asset_info:
+            return
         self.base = asset_info.base
         self.currency = asset_info.currency
         self.symbol = self.accnt.make_ticker_id(self.base, self.currency)
