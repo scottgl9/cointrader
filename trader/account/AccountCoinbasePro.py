f5044a978f25789854cb9d410315a261ef724d14
Scott Glover <scottgl@gmail.com>
2019-09-25 18:47:01 -0500

    Cleanup of Account classes

--- trader/account/AccountCoinbasePro.py
+++ trader/account/AccountCoinbasePro.py
@@ -507,149 +507,26 @@ class AccountCoinbasePro(AccountBase):
         return type
 
     def parse_order_update(self, result):
-        symbol = None
-        orig_id = None
-        order_id = None
-        side = None
-        order_type = None
-        order_price = 0
-        stop_price = 0
-        order_size = 0
-        order_status = None
-        exec_type = None
-        reject_reason = None
-        ts = 0
-
-        if self.simulate:
-            return None
-
-        # maybe use for debug
-        #self.logger.info("parse_order_update={}".format(result))
-
-        if 'c' in result: order_id = result['c']
-        if 'C' in result: orig_id = result['C']
-        if 'r' in result: reject_reason = result['r']
-        if 's' in result: symbol = result['s']
-        if 'S' in result: side = result['S']
-        if 'o' in result: order_type = result['o']
-        if 'q' in result: order_size = result['q']
-        if 'p' in result: order_price = result['p']
-        if 'P' in result: stop_price = result['P']
-        if 'X' in result: order_status = result['X']
-        if 'x' in result: exec_type = result['x']
-        if 'T' in result: ts = result['T']
-
-        if not symbol:
-            return None
-
-        msg_status = Message.MSG_NONE
-
-        if not order_type:
-            return None
-
-        msg_type = self.get_order_msg_type(order_type)
-
-        if exec_type == 'TRADE' and order_status == 'FILLED':
-            if side == 'BUY':
-                msg_status = Message.MSG_BUY_COMPLETE
-            elif side == 'SELL':
-                msg_status = Message.MSG_SELL_COMPLETE
-        elif exec_type == 'REJECTED' and order_status == 'REJECTED':
-            if side == 'BUY':
-                msg_status = Message.MSG_BUY_FAILED
-            elif side == 'SELL':
-                msg_status = Message.MSG_SELL_FAILED
-
-        order_update = OrderUpdate(symbol, order_price, stop_price, order_size, order_type, exec_type,
-                                   side, ts, order_id, orig_id, order_status, reject_reason, msg_type, msg_status)
-
-        return order_update
+        # order_update = OrderUpdate(symbol, order_price, stop_price, order_size, order_type, exec_type,
+        #                            side, ts, order_id, orig_id, order_status, reject_reason, msg_type, msg_status)
+        #
+        # return order_update
+        pass
 
 
-    # parse json response to binance API order, then use to create Order object
+    # parse json response to API order, then use to create Order object
     def parse_order_result(self, result, symbol=None, sigid=0):
-        orderid = None
-        origqty = 0
-        quoteqty = 0
-        side = None
-        commission = 0
-        status = None
-        prices = []
-        type = None
-        order_type = None
-        symbol = symbol
-
-        if self.simulate:
-            return None
-
-        # maybe use for debug
-        #self.logger.info("parse_order_result={}".format(result))
-
-        if 'orderId' in result: orderid = result['orderId']
-        if 'origQty' in result: origqty = result['origQty']
-        fills = result['fills']
-        if 'cummulativeQuoteQty' in result: quoteqty = float(result['cummulativeQuoteQty'])
-        if 'symbol' in result: symbol = result['symbol']
-        if 'status' in result: status = result['status']
-        if 'type' in result: order_type = result['type']
-        if 'side' in result: side = result['side']
-        if 'price' in result and float(result['price']) != 0:
-            prices.append(float(result['price']))
-
-        for fill in fills:
-            if 'side' in fill: side = fill['side']
-            if 'type' in fill: order_type = fill['type']
-            if 'status' in fill: status = fill['status']
-            if 'price' in fill and float(fill['price']) != 0:
-                prices.append(float(result['price']))
-            if 'type' in fill: order_type = fill['type']
-            if 'symbol' in fill: symbol = fill['symbol']
-            if 'commission' in fill: commission = fill['commission']
-
-        if not symbol or (status != 'FILLED' and status != 'CANCELED'):
-            return None
-
-        if not side or not order_type:
-            return None
-
-        price = 0
-        if len(prices) != 0:
-            if side == 'BUY':
-                price = max(prices)
-            elif side == 'SELL':
-                price = min(prices)
-
-        side_type = Order.SIDE_UNKNOWN
-        if side == 'BUY':
-            side_type = Order.SIDE_BUY
-        elif side == 'SELL':
-            side_type = Order.SIDE_SELL
-
-        if status == 'FILLED':
-            type = self.get_order_msg_cmd(order_type, side)
-        elif status == 'CANCELED' and side == 'BUY':
-            type = Message.MSG_BUY_CANCEL
-        elif status == 'CANCELED' and side == 'SELL':
-            type = Message.MSG_SELL_CANCEL
-        elif status == 'REJECTED' and side == 'BUY':
-            type = Message.MSG_BUY_FAILED
-        elif status == 'REJECTED' and side == 'SELL':
-            type = Message.MSG_SELL_FAILED
-
-        order = Order(symbol=symbol,
-                      price=price,
-                      size=origqty,
-                      type=type,
-                      side=side_type,
-                      orderid=orderid,
-                      quote_size=quoteqty,
-                      commission=commission,
-                      sig_id=sigid)
-
-        # maybe use for debug
-        #self.logger.info("order: {}".format(str(order)))
-        return order
-
+        # order = Order(symbol=symbol,
+        #               price=price,
+        #               size=origqty,
+        #               type=type,
+        #               orderid=orderid,
+        #               quote_size=quoteqty,
+        #               commission=commission,
+        #               sig_id=sigid)
+        #
+        # return order
+        pass
 
     # determine if asset has disabled deposits, if so don't trade
     def deposit_asset_disabled(self, name):
