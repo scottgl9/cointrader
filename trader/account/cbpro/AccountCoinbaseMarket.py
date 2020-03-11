fc2989ce36182a7170850fdb0092bac85f6e53fc
Scott Glover <scottgl@gmail.com>
2020-03-10 19:11:03 -0500

    Moved account coinbase market code to submodule AccountCoinbaseMarket

---
+++ trader/account/cbpro/AccountCoinbaseMarket.py
@@ -0,0 +1,162 @@
+from trader.account.AccountBaseMarket import AccountBaseMarket
+from .cbpro import AuthenticatedClient, PublicClient
+from datetime import datetime, timedelta
+import time
+import aniso8601
+import stix.utils.dates
+
+class AccountCoinbaseMarket(AccountBaseMarket):
+    def __init__(self, client, info, simulation=False, logger=None):
+        self.client = client
+        self.info = info
+        self.simulate = simulation
+        self.logger = logger
+        self._tickers = {}
+        self._min_tickers = {}
+        self._max_tickers = {}
+        self.pc = PublicClient()
+
+    def ts_to_iso8601(self, ts):
+        dt = datetime.fromtimestamp(ts)
+        return stix.utils.dates.serialize_value(dt)
+
+    def get_ticker(self, symbol=None):
+        if not self.simulate:
+            if symbol:
+                result = self.client.get_product_ticker(product_id=symbol)
+                if result:
+                    try:
+                        price = float(result['price'])
+                    except KeyError:
+                        price = 0.0
+                    return price
+            #elif not len(self._tickers):
+            #    self._tickers = self.get_all_tickers()
+        try:
+            price = self._tickers[symbol]
+        except KeyError:
+            price = 0.0
+        return price
+
+    def get_tickers(self):
+        return self._tickers
+
+    def get_ticker_symbols(self, currency=None):
+        result = []
+        if not self.simulate:
+            products = self.pc.get_products()
+            for product in products:
+                if currency and product['id'].endswith(currency):
+                    result.append(product['id'])
+                else:
+                    result.append(product['id'])
+        else:
+            result = self.info.get_info_all_assets().keys()
+        return result
+
+    def get_min_tickers(self):
+        return self._min_tickers
+
+    def get_max_tickers(self):
+        return self._max_tickers
+
+    def update_ticker(self, symbol, price, ts):
+        if self.simulate:
+            last_price = 0
+            try:
+                last_price = self._tickers[symbol]
+            except KeyError:
+                pass
+
+            if not last_price:
+                self._min_tickers[symbol] = [price, ts]
+                self._max_tickers[symbol] = [price, ts]
+            else:
+                if price < self._min_tickers[symbol][0]:
+                    self._min_tickers[symbol] = [price, ts]
+                elif price > self._max_tickers[symbol][0]:
+                    self._max_tickers[symbol] = [price, ts]
+
+        self._tickers[symbol] = price
+
+    def update_tickers(self, tickers):
+        for symbol, price in tickers.items():
+            self._tickers[symbol] = float(price)
+
+    # The granularity field must be one of the following values: {60, 300, 900, 3600, 21600, 86400}
+    # The maximum amount of data returned is 300 candles
+    # kline format: [ timestamp, low, high, open, close, volume ]
+    def get_klines(self, days=0, hours=1, ticker_id=None):
+        end = datetime.utcnow()
+        start = (end - timedelta(days=days, hours=hours))
+        granularity = 900
+        if days == 0 and hours < 6:
+            granularity = 60
+        elif (days== 0 and hours <= 24) or (days == 1 and hours == 0):
+            granularity = 300
+
+        rates = self.pc.get_product_historic_rates(ticker_id,
+                                                   start.isoformat(),
+                                                   end.isoformat(),
+                                                   granularity=granularity)
+        return rates[::-1]
+
+
+    def get_hourly_klines(self, symbol, start_ts, end_ts, reverse=False):
+        result = []
+        if not reverse:
+            ts1 = start_ts
+            ts2 = end_ts
+            while ts1 <= end_ts:
+                ts2 = end_ts
+                if (ts2 - ts1) > 3600 * 250:
+                    ts2 = ts1 + 3600 * 250
+                start = self.ts_to_iso8601(ts1)
+                end = self.ts_to_iso8601(ts2)
+
+                klines = self.pc.get_product_historic_rates(product_id=symbol,
+                                                            start=start,
+                                                            end=end,
+                                                            granularity=3600)
+                ts1 = ts2 + 3600
+                if isinstance(klines, list):
+                    try:
+                        if len(klines):
+                            result += reversed(klines)
+                    except TypeError:
+                        print(klines, type(klines))
+                        pass
+                    time.sleep(1)
+                else:
+                    if klines['message'] == 'NotFound':
+                        time.sleep(1)
+                        continue
+                    print("ERROR get_hourly_klines(): {}".format(klines['message']))
+                    return result
+        # else:
+        #     ts1 = start_ts
+        #     ts2 = end_ts
+        #     while ts1 >= start_ts:
+        #         ts1 = start_ts
+        #         if (ts2 - ts1) > 3600 * 250:
+        #             ts1 = ts2 - 3600 * 250
+        #         start = self.ts_to_iso8601(ts1)
+        #         end = self.ts_to_iso8601(ts2)
+        #         klines = self.pc.get_product_historic_rates(product_id=symbol,
+        #                                                     start=start,
+        #                                                     end=end,
+        #                                                     granularity=3600)
+        #         ts1 = ts2 - 3600
+        #         if isinstance(klines, list):
+        #             try:
+        #                 result = reversed(klines) + result
+        #             except TypeError:
+        #                 pass
+        #             time.sleep(1)
+        #         else:
+        #             if klines['message'] == 'NotFound':
+        #                 break
+        #             print("ERROR get_hourly_klines(): {}".format(klines['message']))
+        #             return result
+
+        return result
--- trader/account/cbpro/AccountCoinbasePro.py
+++ trader/account/cbpro/AccountCoinbasePro.py
@@ -6,6 +6,7 @@ from trader.lib.struct.AssetInfo import AssetInfo
 from .AccountCoinbaseInfo import AccountCoinbaseInfo
 from .AccountCoinbaseBalance import AccountCoinbaseBalance
 from .AccountCoinbaseTrade import AccountCoinbaseTrade
+from .AccountCoinbaseMarket import AccountCoinbaseMarket
 from .cbpro import AuthenticatedClient, PublicClient
 from trader.config import *
 import json
@@ -36,6 +37,7 @@ class AccountCoinbasePro(AccountBase):
         self.info = AccountCoinbaseInfo(client, simulation, logger, self.exchange_info_file)
         self.balance = AccountCoinbaseBalance(client, simulation, logger)
         self.trade = AccountCoinbaseTrade(client, simulation, logger)
+        self.market = AccountCoinbaseMarket(client, self.info, simulation, logger)
 
         # hourly db column names
         self.hourly_cnames = ['ts', 'low', 'high', 'open', 'close', 'volume']
@@ -45,9 +47,6 @@ class AccountCoinbasePro(AccountBase):
         for currency in self.info.get_currencies():
             self._currency_buy_size[currency] = 0
 
-        self._tickers = {}
-        self._min_tickers = {}
-        self._max_tickers = {}
         self._trader_profit_mode = 'BTC'
         self._tpprofit = 0
         self.initial_currency = 0
@@ -72,10 +71,6 @@ class AccountCoinbasePro(AccountBase):
 
     def trade_mode_realtime(self):
         return self._trader_mode == AccountBase.TRADER_MODE_REALTIME
-
-    def ts_to_iso8601(self, ts):
-        dt = datetime.fromtimestamp(ts)
-        return stix.utils.dates.serialize_value(dt)
 
     def format_ts(self, ts):
         return int(ts)
@@ -113,56 +108,6 @@ class AccountCoinbasePro(AccountBase):
     # get hourly db column names
     def get_hourly_column_names(self):
         return self.hourly_cnames
-
-    def get_ticker(self, symbol=None):
-        if not self.simulate:
-            if symbol:
-                result = self.client.get_product_ticker(product_id=symbol)
-                if result:
-                    try:
-                        price = float(result['price'])
-                    except KeyError:
-                        price = 0.0
-                    return price
-            #elif not len(self._tickers):
-            #    self._tickers = self.get_all_tickers()
-        try:
-            price = self._tickers[symbol]
-        except KeyError:
-            price = 0.0
-        return price
-
-    def update_ticker(self, symbol, price, ts):
-        if self.simulate:
-            last_price = 0
-            try:
-                last_price = self._tickers[symbol]
-            except KeyError:
-                pass
-
-            if not last_price:
-                self._min_tickers[symbol] = [price, ts]
-                self._max_tickers[symbol] = [price, ts]
-            else:
-                if price < self._min_tickers[symbol][0]:
-                    self._min_tickers[symbol] = [price, ts]
-                elif price > self._max_tickers[symbol][0]:
-                    self._max_tickers[symbol] = [price, ts]
-
-        self._tickers[symbol] = price
-
-    def update_tickers(self, tickers):
-        for symbol, price in tickers.items():
-            self._tickers[symbol] = float(price)
-
-    def get_tickers(self):
-        return self._tickers
-
-    def get_min_tickers(self):
-        return self._min_tickers
-
-    def get_max_tickers(self):
-        return self._max_tickers
 
     def set_trader_profit_mode(self, mode):
         if mode in self.info.get_currencies():
@@ -334,103 +279,5 @@ class AccountCoinbasePro(AccountBase):
 
         return total_balance
 
-    def get_all_ticker_symbols(self, currency=None):
-        result = []
-        products = self.pc.get_products()
-        for product in products:
-            if currency and product['id'].endswith(currency):
-                result.append(product['id'])
-            else:
-                result.append(product['id'])
-        return result
-
-    def get_all_tickers(self):
-        pass
-    #    result = {}
-    #    if not self.simulate:
-    #        for ticker in self.client.get_product_ticker()
-    #            result[ticker['symbol']] = ticker['price']
-    #    else:
-    #        result = self._tickers
-    #    return result
-
     def cancel_all(self, ticker_id=None):
         return self.client.cancel_all(product_id=ticker_id)
-
-    # The granularity field must be one of the following values: {60, 300, 900, 3600, 21600, 86400}
-    # The maximum amount of data returned is 300 candles
-    # kline format: [ timestamp, low, high, open, close, volume ]
-    def get_klines(self, days=0, hours=1, ticker_id=None):
-        end = datetime.utcnow()
-        start = (end - timedelta(days=days, hours=hours))
-        granularity = 900
-        if days == 0 and hours < 6:
-            granularity = 60
-        elif (days== 0 and hours <= 24) or (days == 1 and hours == 0):
-            granularity = 300
-
-        rates = self.pc.get_product_historic_rates(ticker_id,
-                                                   start.isoformat(),
-                                                   end.isoformat(),
-                                                   granularity=granularity)
-        return rates[::-1]
-
-
-    def get_hourly_klines(self, symbol, start_ts, end_ts, reverse=False):
-        result = []
-        if not reverse:
-            ts1 = start_ts
-            ts2 = end_ts
-            while ts1 <= end_ts:
-                ts2 = end_ts
-                if (ts2 - ts1) > 3600 * 250:
-                    ts2 = ts1 + 3600 * 250
-                start = self.ts_to_iso8601(ts1)
-                end = self.ts_to_iso8601(ts2)
-
-                klines = self.pc.get_product_historic_rates(product_id=symbol,
-                                                            start=start,
-                                                            end=end,
-                                                            granularity=3600)
-                ts1 = ts2 + 3600
-                if isinstance(klines, list):
-                    try:
-                        if len(klines):
-                            result += reversed(klines)
-                    except TypeError:
-                        print(klines, type(klines))
-                        pass
-                    time.sleep(1)
-                else:
-                    if klines['message'] == 'NotFound':
-                        time.sleep(1)
-                        continue
-                    print("ERROR get_hourly_klines(): {}".format(klines['message']))
-                    return result
-        # else:
-        #     ts1 = start_ts
-        #     ts2 = end_ts
-        #     while ts1 >= start_ts:
-        #         ts1 = start_ts
-        #         if (ts2 - ts1) > 3600 * 250:
-        #             ts1 = ts2 - 3600 * 250
-        #         start = self.ts_to_iso8601(ts1)
-        #         end = self.ts_to_iso8601(ts2)
-        #         klines = self.pc.get_product_historic_rates(product_id=symbol,
-        #                                                     start=start,
-        #                                                     end=end,
-        #                                                     granularity=3600)
-        #         ts1 = ts2 - 3600
-        #         if isinstance(klines, list):
-        #             try:
-        #                 result = reversed(klines) + result
-        #             except TypeError:
-        #                 pass
-        #             time.sleep(1)
-        #         else:
-        #             if klines['message'] == 'NotFound':
-        #                 break
-        #             print("ERROR get_hourly_klines(): {}".format(klines['message']))
-        #             return result
-
-        return result
