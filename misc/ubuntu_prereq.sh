75bb21780f16ec93796f140dd78420075af5d6d3
Scott Glover <scottgl@gmail.com>
2020-04-24 17:00:13 -0500

    Relocated binance live plot scripts to tools/live. Added initial robinhood_plot_live_EMA.py

--- misc/ubuntu_prereq.sh
+++ misc/ubuntu_prereq.sh
@@ -2,6 +2,6 @@
 apt-get update
 apt-get install -y python3-pip python3-autobahn python3-flask python3-twisted python3-tz python3-numpy python3-six python3-pyqt4
 apt-get install -y python3-requests python3-matplotlib python3-scipy python3-sklearn python3-pymongo python3-aniso8601
-apt-get install -y python3-websocket cython python3-pandas python3-psycopg2 python3-cryptography python3-gevent python3-crontab python3-dateparser python3-pyotp
+apt-get install -y python3-websocket cython python3-pandas python3-psycopg2 python3-cryptography python3-gevent python3-crontab python3-pyotp
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
