e306942c4b200bd834d6a536a2386f548c00ef5a
Scott Glover <scottgl@gmail.com>
2020-11-10 16:30:07 -0600

    Updates to robinhood trade plugin

--- tools/robinhood/robinhood_stocks_total_value.py
+++ tools/robinhood/robinhood_stocks_total_value.py
@@ -39,18 +39,3 @@ if __name__ == '__main__':
     #print(accnt.get_tickers())
     #accnt.get_ticker('BTC-USD')
     print(accnt.get_account_balances())
-    #print("USD Total: {} USD".format(accnt.get_account_total_value()))
-    #ts = int(datetime.timestamp(datetime.now()))
-    #print(ts)
-    #print(accnt.ts_to_iso8601(ts))
-    #symbols = accnt.get_all_ticker_symbols()
-    #print(symbols)
-    #accnt.load_exchange_info()
-    #accnt_assets = accnt_info['assets']
-    #assets = sorted(accnt_assets, key=lambda x: (accnt_assets[x]['usd']), reverse=True)
-    #for asset in assets:
-    #    print("{: >5}: {: >15} {: >10} USD\t{: >20} BTC".format(asset, accnt_assets[asset]['amount'], round(accnt_assets[asset]['usd'], 2), accnt_assets[asset]['btc']))
-
-    #print("\nTotal balance USD = {}, BTC={}, BNB={}".format(accnt_info['total']['usd'],
-    #                                                        accnt_info['total']['btc'],
-    #                                                        accnt_info['total']['bnb']))
--- trader/account/robinhood/AccountRobinhoodTrade.py
+++ trader/account/robinhood/AccountRobinhoodTrade.py
@@ -1,4 +1,5 @@
 from trader.account.AccountBaseTrade import AccountBaseTrade
+from trader.lib.struct.Exchange import Exchange
 
 class AccountRobinhoodTrade(AccountBaseTrade):
     def __init__(self, client, info, simulate=False, logger=None):
@@ -8,31 +9,59 @@ class AccountRobinhoodTrade(AccountBaseTrade):
         self.logger = logger
 
     def buy_market(self, size, price=0.0, ticker_id=None):
-        return self.client.order_buy_market(ticker_id, size)
+        mode = self.info.get_account_mode()
+        if mode == Exchange.ACCOUNT_MODE_CRYPTO:
+            return self.client.order_buy_crypto_by_quantity(ticker_id, size)
+        elif mode == Exchange.ACCOUNT_MODE_STOCKS:
+            return self.client.order_buy_market(ticker_id, size)
 
     def sell_market(self, size, price=0.0, ticker_id=None):
-        return self.client.order_sell_market(ticker_id, size)
+        mode = self.info.get_account_mode()
+        if mode == Exchange.ACCOUNT_MODE_CRYPTO:
+            return self.client.order_sell_crypto_by_quantity(ticker_id, size)
+        elif mode == Exchange.ACCOUNT_MODE_STOCKS:
+            return self.client.order_sell_market(ticker_id, size)
 
     def buy_limit(self, price, size, ticker_id=None):
-        return self.client.order_buy_limit(ticker_id, size, price)
+        mode = self.info.get_account_mode()
+        if mode == Exchange.ACCOUNT_MODE_CRYPTO:
+            return self.client.order_buy_crypto_limit(ticker_id, size, price)
+        elif mode == Exchange.ACCOUNT_MODE_STOCKS:
+            return self.client.order_buy_limit(ticker_id, size, price)
 
     def sell_limit(self, price, size, ticker_id=None):
-        return self.client.order_sell_limit(ticker_id, size, price)
+        mode = self.info.get_account_mode()
+        if mode == Exchange.ACCOUNT_MODE_CRYPTO:
+            return self.client.order_sell_crypto_limit(ticker_id, size, price)
+        elif mode == Exchange.ACCOUNT_MODE_STOCKS:
+            return self.client.order_sell_limit(ticker_id, size, price)
 
     def buy_limit_stop(self, price, size, stop_price, ticker_id=None):
-        return self.client.order_buy_stop_limit(ticker_id, size, price, stop_price)
+        raise NotImplementedError
 
     def sell_limit_stop(self, price, size, stop_price, ticker_id=None):
-         return self.client.order_sell_stop_limit(ticker_id, size, price, stop_price)
+        raise NotImplementedError
 
     def get_order(self, order_id, ticker_id):
-        return self.client.get_order_info(order_id)
+        mode = self.info.get_account_mode()
+        if mode == Exchange.ACCOUNT_MODE_CRYPTO:
+            return self.client.get_crypto_order_info(order_id)
+        elif mode == Exchange.ACCOUNT_MODE_STOCKS:
+            return self.client.get_stock_order_info(order_id)
 
     def get_orders(self, ticker_id=None):
-        return self.client.get_all_orders()
+        mode = self.info.get_account_mode()
+        if mode == Exchange.ACCOUNT_MODE_CRYPTO:
+            return self.client.get_all_open_crypto_orders()
+        elif mode == Exchange.ACCOUNT_MODE_STOCKS:
+            return self.client.get_all_open_stock_orders()
 
     def cancel_order(self, orderid, ticker_id=None):
-        self.client.cancel_order(orderid)
+        mode = self.info.get_account_mode()
+        if mode == Exchange.ACCOUNT_MODE_CRYPTO:
+            self.client.cancel_crypto_order(orderid)
+        elif mode == Exchange.ACCOUNT_MODE_STOCKS:
+            self.client.cancel_stock_order(orderid)
 
     def parse_order_update(self, result):
         raise NotImplementedError
